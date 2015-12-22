ff8790d18f322ee936158d9fe8cef2b11b7a8bb4
Anubhav Jain <ajain@lbl.gov>
2015-12-22 11:27:43 -0800

    push some comments on mp.py

--- matminer/data_retrieval/mp.py
+++ matminer/data_retrieval/mp.py
@@ -8,11 +8,16 @@ import pandas as pd
 # - Setting the "MAPI_KEY" environment variable.
 # Please obtain your API key at https://www.materialsproject.org/dashboard
 
-m = MPRester()
+# TODO: fix the format of the commenting above. use the triple-quote for multi-line comments.
+
+m = MPRester()  # TODO: put this inside the class. You should NOT create global variables like this unless you know what you are doing!
 
 class MPDataRetrieval():
+    # TODO: description of class goes here. Use FWS format.
 
     def __init__(self, criteria, properties, mp_decode=True):
+        # TODO: description of args goes here. Use FWS format.
+
         self.criteria = criteria
         self.properties = properties
         self.mp_decode = mp_decode
@@ -26,5 +31,7 @@ class MPDataRetrieval():
         df = pd.DataFrame(self.data, columns=self.properties)
         return df
 
-
-
+# TODO: the user has no way to set their API key!! The code will return an error unless the API is set as an environment variable. The MPDataRetrieval should take in the API key as an argument.
+# TODO: add an example in an 'examples' package. This exercise would have probably caught some of the errors.
+# TODO: make it easy to set the task_id as the default index frame. This should be done in almost all instances unless the user asks not to do it.
+# TODO: clean up unused import statements. PyCharm should help you identify what is not needed (greyed out). Others are red underlined (periodic table)
