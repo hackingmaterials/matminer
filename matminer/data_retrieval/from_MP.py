6f580beb4732e0a479490158ef1c29600f3be61a
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2015-12-26 22:19:19 -0800

    Some more PEP

--- matminer/data_retrieval/from_MP.py
+++ matminer/data_retrieval/from_MP.py
@@ -2,22 +2,46 @@ from pymatgen import MPRester
 import pandas as pd
 
 
-# TODO: fix the format of the commenting above. use the triple-quote for multi-line comments.
-
-
 class MPDataRetrieval:
     """
-    as
+    MPDataRetrieval is used to retrieve data from the Materials Project database, print the results, and convert them
+    into an indexed/unindexed Pandas dataframe.
     """
 
     def __init__(self, criteria, properties, api_key=None, mp_decode=True):
-        # TODO: description of args goes here. Use FWS format.
+        """
+        :param criteria (str/dict): Criteria of the query as a string or mongo-style dict.
+            If string, it supports a powerful but simple string criteria.
+            E.g., "Fe2O3" means search for materials with reduced_formula
+            Fe2O3. Wild cards are also supported. E.g., "\*2O" means get
+            all materials whose formula can be formed as \*2O, e.g.,
+            Li2O, K2O, etc.
 
-        # api_key (str): A String API key for accessing the MaterialsProject
-        # Initializing MPRester. Note that you can call MPRester. MPRester looks for the API key in two places:
-        # - Supplying it directly as an __init__ arg.
-        # - Setting the "MAPI_KEY" environment variable.
-        # Please obtain your API key at https://www.materialsproject.org/dashboard
+            Other syntax examples:
+            mp-1234: Interpreted as a Materials ID.
+            Fe2O3 or \*2O3: Interpreted as reduced formulas.
+            Li-Fe-O or \*-Fe-O: Interpreted as chemical systems.
+
+            You can mix and match with spaces, which are interpreted as
+            "OR". E.g. "mp-1234 FeO" means query for all compounds with
+            reduced formula FeO or with materials_id mp-1234.
+
+            Using a full dict syntax, even more powerful queries can be
+            constructed. For example, {"elements":{"$in":["Li",
+            "Na", "K"], "$all": ["O"]}, "nelements":2} selects all Li, Na
+            and K oxides. {"band_gap": {"$gt": 1}} selects all materials
+            with band gaps greater than 1 eV.
+        :param properties (list): Properties to request for as a list. For example, ["formula",
+            "formation_energy_per_atom"] returns the formula and formation energy per atom.
+        :param api_key (str): A String API key for accessing the MaterialsProject. Note that when you call MPRester,
+            it looks for the API key in two places:
+            - Supplying it directly as an __init__ arg.
+            - Setting the "MAPI_KEY" environment variable.
+              Please obtain your API key at https://www.materialsproject.org/dashboard
+        :param mp_decode (bool): Whether to do a decoding to a Pymatgen object
+            where possible. In some cases, it might be useful to just get
+            the raw python dict, i.e., set to False.
+        """
         self.criteria = criteria
         self.properties = properties
         self.api_key = api_key
@@ -32,10 +56,18 @@ class MPDataRetrieval:
             self.properties.append("material_id")
         self.data = mprest.query(self.criteria, self.properties, self.mp_decode)
 
-    def print_output(self):
+    def return_output(self):
+        """
+        :return: results of the query command of pymatgen
+        """
         return self.data
 
     def to_pandas(self, index_mpid=True):
+        """
+        :param index_mpid (bool): Whether to index the data frame on MPID or not. Defaults to True.
+        :return: An indexed or unindexed Pandas data frame of the results.
+        """
+        self.index_mpid = index_mpid
         if index_mpid:
             df = pd.DataFrame(self.data, columns=self.properties)
             indexed_df = df.set_index("material_id")
