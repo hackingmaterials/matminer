094165bd77096d6ef47f2aa513d4d9cefbb15d4b
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-01-14 12:58:42 -0800

    Add function for evaluating max,min range

--- matminer/descriptors/composition_features.py
+++ matminer/descriptors/composition_features.py
@@ -3,13 +3,21 @@ from pymatgen import Element, Composition
 __author__ = 'Anubhav Jain <ajain@lbl.gov>'
 
 
-def get_mass_list(x):
+def get_mass_list(comp):
     el_mass = []
-    el_amt = Composition(x).get_el_amt_dict()
+    el_amt = Composition(comp).get_el_amt_dict()
     for el in el_amt:
         el_mass.append(Element(el).atomic_mass)
     return el_mass
 
+def get_max_min(lst):
+    maxmin_dict = {}
+    maxmin_dict['Max'] = max(lst)
+    maxmin_dict['Min'] = min(lst)
+    return maxmin_dict
 
 if __name__ == '__main__':
-    print get_mass_list('PbTe')
+    m = get_mass_list('LiFePO4')
+    print m
+    n = get_max_min(m)
+    print n
