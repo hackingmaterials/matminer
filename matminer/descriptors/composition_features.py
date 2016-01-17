61dfe1516e5e96a41aa6f6d00b02f8a95241018e
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-01-16 17:37:33 -0800

    Add function to calculate mean of a descriptor list

--- matminer/descriptors/composition_features.py
+++ matminer/descriptors/composition_features.py
@@ -1,4 +1,5 @@
 from pymatgen import Element, Composition
+import numpy as np
 
 __author__ = 'Anubhav Jain <ajain@lbl.gov>'
 
@@ -15,9 +16,14 @@ def get_max_min(lst):
     maxmin_dict = {'Max': max(lst), 'Min': min(lst)}
     return maxmin_dict
 
+def get_mean(lst):
+    return np.mean(lst)
+
 
 if __name__ == '__main__':
     m = get_mass_list('LiFePO4')
     print m
     m_n = get_max_min(m)
     print m_n
+    m_m = get_mean(m)
+    print m_m
