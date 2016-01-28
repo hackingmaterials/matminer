5fe767c3f5429f9f01f6d147d799f68c57952bad
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-01-28 12:04:29 -0800

    Getting all images from a page

--- matminer/data_retrieval/retrieve_Springer.py
+++ matminer/data_retrieval/retrieve_Springer.py
@@ -1,19 +1,37 @@
-from lxml import html
 import requests
+from lxml import html
+import sys
+import urlparse
 
 page = requests.get('http://materials.springer.com/isp/crystallographic/docs/sd_0456276')
 print page.raise_for_status()
-tree = html.fromstring(page.content)
+parsed_body = html.fromstring(page.content)
+
+print parsed_body
 
 print type(page)
 print page.status_code == requests.codes.ok
 print len(page.text)
 print page.text[:500]
 
-print tree
-
-cif = tree.xpath('//*[@id="action-download-cif-link"]')
+cif = parsed_body.xpath('//*[@id="action-download-cif-link"]')
 print cif
 
-labels = tree.xpath('//*[@id="general_information"]/div[1]/div/div/ul/li[1]/strong/text()')
+labels = parsed_body.xpath('//*[@id="general_information"]/div[1]/div/div/ul/li[1]/strong/text()')
 print labels
+
+# Grab links to all images
+images = parsed_body.xpath('//img/@src')
+if not images:
+    sys.exit("Found No Images")
+
+# Convert any relative urls to absolute urls
+images = [urlparse.urljoin(page.url, url) for url in images]
+print 'Found %s images' % len(images)
+
+# Only download first 10
+for url in images[0:10]:
+    r = requests.get(url)
+    f = open('%s' % url.split('/')[-1], 'w')
+    f.write(r.content)
+    f.close()
