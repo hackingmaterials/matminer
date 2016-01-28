8cd1eca419bbf5ae93c280635a2dbe0cc7412f62
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-01-28 11:27:54 -0800

    Checking http requests

--- matminer/data_retrieval/retrieve_Springer.py
+++ matminer/data_retrieval/retrieve_Springer.py
@@ -4,6 +4,11 @@ import requests
 page = requests.get('http://materials.springer.com/isp/crystallographic/docs/sd_0456276')
 tree = html.fromstring(page.content)
 
+print type(page)
+print page.status_code == requests.codes.ok
+print len(page.text)
+print page.text[:500]
+
 print tree
 
 cif = tree.xpath('//*[@id="action-download-cif-link"]')
