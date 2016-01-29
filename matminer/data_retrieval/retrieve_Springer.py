833ec21b296dd552565a6b21b9cd82ae85e9cfda
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-01-28 16:53:51 -0800

    Using lxml instead of BeautifulSoup to download cif, and code cleanup

--- matminer/data_retrieval/retrieve_Springer.py
+++ matminer/data_retrieval/retrieve_Springer.py
@@ -1,53 +1,30 @@
 import requests
 from lxml import html
-import sys
-import urlparse
-import urllib2
-from bs4 import BeautifulSoup
-import urllib
 
 page = requests.get('http://materials.springer.com/isp/crystallographic/docs/sd_0456276')
-print page.raise_for_status()
+print page.raise_for_status()      # Check if getting data from above was successful or now
+print page.status_code == requests.codes.ok      # Check if getting data from above was successful or now
+
 parsed_body = html.fromstring(page.content)
-
-print parsed_body
-
-print type(page)
-print page.status_code == requests.codes.ok
-print len(page.text)
-print page.text[:100]
-
-cif = parsed_body.xpath('//*[@id="action-download-cif-link"]/text()')
-print cif
-
 labels = parsed_body.xpath('//*[@id="general_information"]/div[1]/div/div/ul/li[1]/strong/text()')
 print labels
 
-# Grab links to all images
-images = parsed_body.xpath('//img/@src')
-if not images:
-    sys.exit("Found No Images")
+for a_link in parsed_body.xpath('//a/@href'):
+    if '.cif' in a_link:
+        cif_link = a_link
 
-# Convert any relative urls to absolute urls
-images = [urlparse.urljoin(page.url, url) for url in images]
-print 'Found %s images' % len(images)
+res = requests.get('http://materials.springer.com' + cif_link)
 
-# Only download first 10
-for url in images[0:10]:
-    r = requests.get(url)
-    f = open('%s' % url.split('/')[-1], 'w')
-    f.write(r.content)
-    f.close()
-
-resp = urllib2.urlopen('http://materials.springer.com/isp/crystallographic/docs/sd_0456276')
-soup = BeautifulSoup(resp.read(), 'lxml')
-links = soup.find_all('a')
-print links[5].get('href')
-
-# res = urllib.urlopen('http://materials.springer.com' + links[5].get('href'))
-res = requests.get('http://materials.springer.com' + links[5].get('href'))
-
-with open('CifFile', 'wb') as cif_file:
+with open('ciffile.txt', 'wb') as cif_file:
     cif_file.write(res.content)
 
 cif_file.close()
+
+# Grab links to all images
+# images = parsed_body.xpath('//img/@src')
+# if not images:
+#     sys.exit("Found No Images")
+# Convert any relative urls to absolute urls
+# images = [urlparse.urljoin(page.url, url) for url in images]
+# print 'Found %s images' % len(images)
+
