9bf04a757130bb4a51943f7166a3c9f51989a814
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-01-29 18:06:21 -0500

    Testing database for blockages

--- matminer/data_retrieval/retrieve_Springer.py
+++ matminer/data_retrieval/retrieve_Springer.py
@@ -1,22 +1,20 @@
 import requests
 from lxml import html
 
-page = requests.get('http://materials.springer.com/isp/crystallographic/docs/sd_0456276')
-print page.raise_for_status()      # Check if getting data from above was successful or now
-print page.status_code == requests.codes.ok      # Check if getting data from above was successful or now
+for i in range(2000000, 2025000):
+    page = requests.get('http://materials.springer.com/isp/crystallographic/docs/sd_' + str(i))
+# print page.raise_for_status()      # Check if getting data from above was successful or now
+# print page.status_code == requests.codes.ok      # Check if getting data from above was successful or now
+    parsed_body = html.fromstring(page.content)
+# labels = parsed_body.xpath('//*[@id="general_information"]/div[1]/div/div/ul/li[1]/strong/text()')
+# print labels
 
-parsed_body = html.fromstring(page.content)
-labels = parsed_body.xpath('//*[@id="general_information"]/div[1]/div/div/ul/li[1]/strong/text()')
-print labels
-
-for a_link in parsed_body.xpath('//a/@href'):
-    if '.cif' in a_link:
-        cif_link = a_link
-
-res = requests.get('http://materials.springer.com' + cif_link)
-
-with open('ciffile.txt', 'wb') as cif_file:
-    cif_file.write(res.content)
+    for a_link in parsed_body.xpath('//a/@href'):
+        if '.cif' in a_link:
+            cif_link = a_link
+            res = requests.get('http://materials.springer.com' + cif_link)
+            with open('ciffile.txt', 'a') as cif_file:
+                cif_file.write(res.content)
 
 cif_file.close()
 
@@ -27,4 +25,3 @@ cif_file.close()
 # Convert any relative urls to absolute urls
 # images = [urlparse.urljoin(page.url, url) for url in images]
 # print 'Found %s images' % len(images)
-
