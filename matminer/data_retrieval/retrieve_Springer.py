3a07e9af162366b7df7d8cd6ec2f5e63f0c69102
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-02 09:59:49 -0800

    Testing Springer for 25k http requests

--- matminer/data_retrieval/retrieve_Springer.py
+++ matminer/data_retrieval/retrieve_Springer.py
@@ -1,22 +1,33 @@
 import requests
 from lxml import html
 
-for i in range(2000000, 2025000):
-    page = requests.get('http://materials.springer.com/isp/crystallographic/docs/sd_' + str(i))
-# print page.raise_for_status()      # Check if getting data from above was successful or now
-# print page.status_code == requests.codes.ok      # Check if getting data from above was successful or now
-    parsed_body = html.fromstring(page.content)
-# labels = parsed_body.xpath('//*[@id="general_information"]/div[1]/div/div/ul/li[1]/strong/text()')
-# print labels
+i = 0
+j = 1402650
+while i <= 25000:
+    try:
+        page = requests.get('http://materials.springer.com/isp/crystallographic/docs/sd_' + str(j))
+        if page.raise_for_status() is None:      # Check if getting data from above was successful or now
+            print 'Success at getting sd_' + str(j)
+            i += 1
+            parsed_body = html.fromstring(page.content)
+            for a_link in parsed_body.xpath('//a/@href'):
+                if '.cif' in a_link:
+                    cif_link = a_link
+                    res = requests.get('http://materials.springer.com' + cif_link)
+                    with open('ciffile.txt', 'a') as cif_file:
+                        cif_file.write(res.content)
+    except Exception as e:
+        print e
+        print 'Error in getting sd_' + str(j)
+    j += 1
 
-    for a_link in parsed_body.xpath('//a/@href'):
-        if '.cif' in a_link:
-            cif_link = a_link
-            res = requests.get('http://materials.springer.com' + cif_link)
-            with open('ciffile.txt', 'a') as cif_file:
-                cif_file.write(res.content)
 
 cif_file.close()
+
+# print page.status_code == requests.codes.ok      # Check if getting data from above was successful or now
+
+# labels = parsed_body.xpath('//*[@id="general_information"]/div[1]/div/div/ul/li[1]/strong/text()')
+# print labels
 
 # Grab links to all images
 # images = parsed_body.xpath('//img/@src')
