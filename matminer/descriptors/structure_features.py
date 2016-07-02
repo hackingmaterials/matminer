9b1791d3f87a0f04fab30717be989b3dac8f4185
Anubhav Jain <ajain@lbl.gov>
2016-07-01 17:34:28 -0700

    add a couple of extremely simple structure descriptors

--- matminer/descriptors/structure_features.py
+++ matminer/descriptors/structure_features.py
@@ -1,1 +1,25 @@
+from __future__ import division
+from monty import math
+
 __author__ = 'Anubhav Jain <ajain@lbl.gov>'
+
+
+def get_packing_fraction(s):
+    if not s.is_ordered:
+        raise ValueError("Disordered structure support not built yet")
+    total_rad = 0
+    for site in s:
+        total_rad += site.specie.atomic_radius**2 * math.pi
+
+    return total_rad/s.volume
+
+
+def get_vol_per_site(s):
+    if not s.is_ordered:
+        raise ValueError("Disordered structure support not built yet")
+
+    return s.volume / len(s)
+
+
+def density(s):
+    return s.density
