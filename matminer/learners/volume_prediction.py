cdc80d1325d1949882a1717d32e8a941834fd4e7
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-07-05 22:48:52 -0700

    modify docs and correct ionic_factor

--- matminer/learners/volume_prediction.py
+++ matminer/learners/volume_prediction.py
@@ -3,17 +3,19 @@ from matminer.descriptors.composition_features import get_pymatgen_eldata_lst, g
 
 
 class VolumePredictor:
+    """
+    Predicts volume; given a structure, finds the minimum volume such that
+    no two sites are closer than a linear combination of their atomic and ionic radii.
+    """
     def __init__(self, cutoff=4, ionic_factor=0.30):
         """
-        Predicts volume; given a structure, finds the minimum volume such that
-        no two sites are closer than sum of their atomic radii. The sum of
-        atomic radii is modified for ionicity using an ionic_factor that
-        depends on electronegativity difference.
-        Args:
-            cutoff: (float) cutoff for site pairs (added to site radius)
+        :param cutoff: (float) cutoff for site pairs (added to site radius)
                 in Angstrom. Increase if your initial structure guess
                 is extremely bad (atoms way too far apart). In all other cases,
                 increasing cutoff gives same answer but at lower performance.
+        :param ionic_factor: (float) Factor that accounts for ionicity in a bond.
+                It determines the contribution of ionic and atomic radii of each element
+                making up a bond to the sum of their radii.
         """
         self.cutoff = cutoff
         if ionic_factor > 0.40:
@@ -22,13 +24,13 @@ class VolumePredictor:
 
     def predict(self, structure):
         """
-        Given a structure, returns back a volume
-        Args:
-            structure: (Structure)
-        Returns:
-            (float) expected volume of structure
-        """
+        Given a structure, returns back the structure scaled to predicted volume.
+        Volume is predicted based on minimum bond distance, which is determined using
+        an ionic mix factor based on electronegativity spread in a structure.
 
+        :param structure: pymatgen structure object
+        :return: scaled pymatgen structure object
+        """
         if not structure.is_ordered:
             raise ValueError("VolumePredictorSimple requires "
                              "ordered structures!")
@@ -45,12 +47,10 @@ class VolumePredictor:
                 for site2, dist in x:
                     el2 = site2.specie
                     if el2.atomic_radius:
-                        r1 = el1.average_ionic_radius * ionic_mix +\
-                             el1.atomic_radius * (1-ionic_mix) if \
+                        r1 = el1.average_ionic_radius * ionic_mix + el1.atomic_radius * (1-ionic_mix) if \
                             el1.average_ionic_radius else el1.atomic_radius
 
-                        r2 = el2.average_ionic_radius * ionic_mix +\
-                             el2.atomic_radius * (1-ionic_mix) if \
+                        r2 = el2.average_ionic_radius * ionic_mix + el2.atomic_radius * (1-ionic_mix) if \
                             el2.average_ionic_radius else el2.atomic_radius
 
                         expected_dist = float(r1 + r2)
@@ -59,22 +59,11 @@ class VolumePredictor:
                                 < smallest_distance:
                             smallest_distance = dist/expected_dist
                     else:
-                        warnings.warn("VolumePredictor: no atomic radius data for {}".
-                              format(el2))
-
+                        warnings.warn("VolumePredictor: no atomic radius data for {}".format(el2))
             else:
-                warnings.warn("VolumePredictor: no atomic radius data for {}".
-                              format(el1))
+                warnings.warn("VolumePredictor: no atomic radius data for {}".format(el1))
 
         if not smallest_distance:
             raise ValueError("Could not find any bonds in this material!")
 
         return structure.volume * (1/smallest_distance)**3
-
-
-
-
-
-
-
-
