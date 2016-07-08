537ca18dfa269f671f43a54193e583c4eba10f8b
Anubhav Jain <ajain@lbl.gov>
2016-07-08 01:14:09 -0700

    fix line lengths

--- matminer/learners/volume_prediction.py
+++ matminer/learners/volume_prediction.py
@@ -9,7 +9,8 @@ class VolumePredictor:
     """
     Predicts volume; given a structure, finds the minimum volume such that
     two sites are closer than a weighted sum of their atomic and ionic radii..
-    When run over all stable elemental and binary structures from MP, it is found that:
+    When run over all stable elemental and binary structures from MP,
+    it is found that:
     (i) RMSE % error = 23.6 %
     (ii) Average percentage difference in volume from initial volume = 2.88 %
     (iii) Average absolute difference in volume from initial volume = 17.0 %
@@ -19,13 +20,14 @@ class VolumePredictor:
     def __init__(self, cutoff=4, ionic_factor=0.30):
         """
         :param cutoff: (float) cutoff for site pairs (added to site radius)
-                in Angstrom. Increase if your initial structure guess
-                is extremely bad (atoms way too far apart). In all other cases,
-                increasing cutoff gives same answer but at lower performance.
-        :param ionic_factor: (float) Factor that accounts for ionicity in a bond.
-                It determines the contribution of ionic and atomic radii of each element
-                making up a bond to the sum of their radii.
+            in Angstrom. Increase if your initial structure guess is extremely
+            bad (atoms way too far apart). In all other cases, increasing
+            cutoff gives same answer but takes more time.
+        :param ionic_factor: (float) Factor that accounts for ionicity in a
+            bond. It determines the contribution of ionic and atomic radii of
+            each element making up a bond to the sum of their radii.
         """
+
         self.cutoff = cutoff
         if ionic_factor > 0.40:
             raise ValueError("specified ionic factor is out of range!")
@@ -45,21 +47,24 @@ class VolumePredictor:
             raise ValueError("VolumePredictorSimple requires "
                              "ordered structures!")
 
-        smallest_ratio = None  # smallest ratio of observed vs expected bond distance
-        ionic_mix = get_std(get_element_data(structure.composition, 'X')) * self.ionic_factor
+        smallest_ratio = None  # ratio of observed vs expected bond distance
+        ionic_mix = get_std(get_element_data(structure.composition, 'X')) * \
+                    self.ionic_factor
 
         for site in structure:
             el1 = site.specie
             if el1.atomic_radius:
                 neighbors = structure.get_neighbors(site,
                                             el1.atomic_radius + self.cutoff)
-                r1 = el1.average_ionic_radius * ionic_mix + el1.atomic_radius * (1-ionic_mix) if \
+                r1 = el1.average_ionic_radius * ionic_mix + \
+                     el1.atomic_radius * (1-ionic_mix) if \
                     el1.average_ionic_radius else el1.atomic_radius
 
                 for site2, dist in neighbors:
                     el2 = site2.specie
                     if el2.atomic_radius:
-                        r2 = el2.average_ionic_radius * ionic_mix + el2.atomic_radius * (1-ionic_mix) if \
+                        r2 = el2.average_ionic_radius * ionic_mix + \
+                             el2.atomic_radius * (1-ionic_mix) if \
                             el2.average_ionic_radius else el2.atomic_radius
 
                         expected_dist = float(r1 + r2)
@@ -68,9 +73,11 @@ class VolumePredictor:
                                 < smallest_ratio:
                             smallest_ratio = dist/expected_dist
                     else:
-                        warnings.warn("VolumePredictor: no atomic radius data for {}".format(el2))
+                        warnings.warn("VolumePredictor: no atomic radius data "
+                                      "for {}".format(el2))
             else:
-                warnings.warn("VolumePredictor: no atomic radius data for {}".format(el1))
+                warnings.warn("VolumePredictor: no atomic radius data for "
+                              "{}".format(el1))
 
         if not smallest_ratio:
             raise ValueError("Could not find any bonds in this material!")
@@ -81,8 +88,8 @@ class VolumePredictor:
 
     def get_predicted_structure(self, structure):
         """
-        Given a structure, returns back the structure scaled to predicted volume
-        using the "predict" method.
+        Given a structure, returns back the structure scaled to predicted
+        volume using the "predict" method.
         :param structure: pymatgen Structure object
         :return: scaled pymatgen Structure object
         """
