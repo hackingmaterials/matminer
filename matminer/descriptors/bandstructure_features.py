bc58e2b6d02ea2b1dfe65626d3946f7ec8c83a1d
Anubhav Jain <ajain@lbl.gov>
2016-08-17 12:48:22 -0400

    add basic branch point energy code
    Still needs kpoint weights

--- matminer/descriptors/bandstructure_features.py
+++ matminer/descriptors/bandstructure_features.py
@@ -1,1 +1,45 @@
+from __future__ import division
+
 __author__ = 'Anubhav Jain <ajain@lbl.gov>'
+
+
+def branch_point_energy(bs, n_vb=1, n_cb=1):
+    """
+    Get the branch point energy as defined by:
+    Schleife, Fuchs, Rodi, Furthmuller, Bechstedt, APL 94, 012104 (2009)
+
+    TODO: incorporate kpoint weights!!
+
+    Args:
+        bs: (BandStructure)
+        n_vb: number of valence bands to include
+        n_cb: number of conduction bands to include
+
+    Returns: (int) branch point energy on same energy scale as BS eigenvalues
+
+    """
+    # TODO: add kpoint weights properly!!
+
+    if bs.is_metal():
+        raise ValueError("Cannot define a branch point energy for metals!")
+
+    total_sum_energies = 0
+    num_points = 0
+
+    for spin in bs.bands:
+        for kpt_idx in xrange(len(bs.kpoints)):
+            vb_energies = []
+            cb_energies = []
+            for band_idx in xrange(bs.nb_bands):
+                e = bs.bands[spin][band_idx][kpt_idx]
+                if e > bs.efermi:
+                    cb_energies.append(e)
+                else:
+                    vb_energies.append(e)
+            vb_energies.sort(reverse=True)
+            cb_energies.sort()
+            total_sum_energies += sum(vb_energies[0:n_vb]) + \
+                                  sum(cb_energies[0:n_cb])
+            num_points += n_vb + n_cb
+
+    return total_sum_energies/num_points
