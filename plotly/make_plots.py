99135e8dc5f1c7ecbfb53204bda4755652d7b804
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-08-24 15:21:44 -0700

    add plotting modes for offline and notebook plotting

--- plotly/make_plots.py
+++ plotly/make_plots.py
@@ -4,14 +4,15 @@ __author__ = 'Saurabh Bajaj <sbajaj@lbl.gov>'
 
 
 class ScatterPlot:
-
-    def __init__(self, plot_title=None, x_title=None, y_title=None, mode='markers', hovermode='closest', filename=None):
+    def __init__(self, plot_title=None, x_title=None, y_title=None, mode='markers', hovermode='closest', filename=None,
+                 plot_mode='offline'):
         self.title = plot_title
         self.x_title = x_title
         self.y_title = y_title
         self.mode = mode
         self.hovermode = hovermode
         self.filename = filename
+        self.plot_mode = plot_mode
 
     def plot_dataframe(self, dataframe, x_col, y_col, text_col=None):
         fig = {
@@ -30,7 +31,12 @@ class ScatterPlot:
             }
         }
 
-        if self.filename:
-            plotly.offline.plot(fig, filename=self.filename)
-        else:
-            plotly.offline.plot(fig)
+        if self.plot_mode == 'offline':
+            if self.filename:
+                plotly.offline.plot(fig, filename=self.filename)
+            else:
+                plotly.offline.plot(fig)
+        elif self.plot_mode == 'notebook':
+            plotly.offline.init_notebook_mode()  # run at the start of every notebook; version 1.9.4 required
+            plotly.offline.iplot(fig)
+            # elif self.plot_mode == 'online':
