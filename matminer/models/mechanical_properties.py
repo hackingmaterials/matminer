2144e75aa34296b764c41db74900a82830a1b07b
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-08-29 13:10:22 -0700

    add thermal stress descriptor

--- matminer/models/mechanical_properties.py
+++ matminer/models/mechanical_properties.py
@@ -14,5 +14,24 @@ def vickers_hardness(G, K):
     """
     return 2*(G**3.0/K**2.0)**0.585 - 3
 
+
+def thermal_stress(E_T, alpha_T, delta_T, nu_T):
+    """
+    Calculate thermal stress.
+
+    References:
+        DOI: 10.1007/s10853-013-7569-1 (Room temperature mechanical properties of natural-mineral-based thermoelectrics)
+
+    Args:
+        E_T: (float) temperature-dependent Young's modulus
+        alpha_T: (float) temperature-dependent thermal expansion coefficient
+        delta_T: (float) temperature difference
+        nu_T: (float) temperature-dependent Poisson's ratio
+
+    Returns: (float) thermal stress induced by thermal gradients or thermal transients
+
+    """
+    return (E_T * alpha_T * delta_T)/(1 - nu_T)
+
 if __name__ == "__main__":
     print vickers_hardness(3, 2)
