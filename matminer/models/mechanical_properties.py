c2cb0aecf4233c6bd99d4a793ce2f68dbc557575
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-08-29 13:44:58 -0700

    add fracture toughness descriptor

--- matminer/models/mechanical_properties.py
+++ matminer/models/mechanical_properties.py
@@ -3,7 +3,7 @@ __author__ = 'Saurabh Bajaj <sbajaj@lbl.gov>'
 
 def vickers_hardness1(G, K):
     """
-    Calculate Vickers hardness for a material.
+    Calculate Vickers hardness (H_v) for a material.
 
     References:
         DOI: (not available yet) (Title: "A Statistical Learning Framework for Materials Science: Application to
@@ -13,7 +13,7 @@ def vickers_hardness1(G, K):
         G: (float) elastic bulk moduli
         K: (float) elastic shear moduli
 
-    Returns: (float) Vickers hardness
+    Returns: (float) Vickers hardness (H_v)
 
     """
     return 2*(G**3.0/K**2.0)**0.585 - 3
@@ -21,7 +21,7 @@ def vickers_hardness1(G, K):
 
 def vickers_hardness2(F, a):
     """
-    Calculate Vickers hardness for a material.
+    Calculate Vickers hardness (H) for a material.
 
     References:
         DOI: 10.1007/s10853-013-7569-1 (Title: "Room temperature mechanical properties of natural-mineral-based
@@ -31,7 +31,7 @@ def vickers_hardness2(F, a):
         F: (float) indentation load (N)
         a: (float) half of diagonal length of the indentation impression (m)
 
-    Returns: (float) Vickers hardness
+    Returns: (float) Vickers hardness (H)
 
     """
     return (1.8544 * F)/((2*a)**2.0)
@@ -39,7 +39,7 @@ def vickers_hardness2(F, a):
 
 def thermal_stress(E_T, alpha_T, delta_T, nu_T):
     """
-    Calculate thermal stress.
+    Calculate thermal stress (sigma_T) in a material.
 
     References:
         DOI: 10.1007/s10853-013-7569-1 (Title: "Room temperature mechanical properties of natural-mineral-based
@@ -51,10 +51,32 @@ def thermal_stress(E_T, alpha_T, delta_T, nu_T):
         delta_T: (float) temperature difference (K)
         nu_T: (float) temperature-dependent Poisson's ratio
 
-    Returns: (float) thermal stress induced by thermal gradients or thermal transients
+    Returns: (float) thermal stress (sigma_T) induced by thermal gradients or thermal transients
 
     """
     return (E_T * alpha_T * delta_T)/(1 - nu_T)
+
+
+def fracture_toughness(F, a, E, c):
+    """
+    Calculate fracture toughness (K_c) of a material.
+
+    References:
+        DOI: 10.1007/s10853-013-7569-1 (Title: "Room temperature mechanical properties of natural-mineral-based
+        thermoelectrics")
+
+    Args:
+        F: (float) indentation load (N)
+        a: (float) half of diagonal length of the indentation impression (m)
+        E: (float) Young's modulus
+        c: (float) hald of radial crack length (m)
+
+    Returns: (float) fracture toughness (K_c) of a material
+
+    """
+    H = vickers_hardness2(F, a)
+    return (0.016 * (E/H)**0.5 * F)/(c**1.5)
+
 
 if __name__ == "__main__":
     print vickers_hardness1(3, 2)
