f043af2b14231a6626edb378d8dd1bb310805de8
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-08-29 12:52:42 -0700

    add docstring to Vickers hardness

--- matminer/models/mechanical_properties.py
+++ matminer/models/mechanical_properties.py
@@ -2,6 +2,16 @@ __author__ = 'Saurabh Bajaj <sbajaj@lbl.gov>'
 
 
 def vickers_hardness(G, K):
+    """
+    Calculate Vickers hardness for a material.
+
+    Args:
+        G: (float) elastic bulk moduli
+        K: (float) elastic shear moduli
+
+    Returns: (float) Vickers hardness
+
+    """
     return 2*(G**3.0/K**2.0)**0.585 - 3
 
 if __name__ == "__main__":
