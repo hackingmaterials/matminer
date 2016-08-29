56b19ea06f70cd5f60123a4f39bae30ed14a2446
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-08-29 13:18:26 -0700

    add 2nd Vickers hardness model

--- matminer/models/mechanical_properties.py
+++ matminer/models/mechanical_properties.py
@@ -19,6 +19,24 @@ def vickers_hardness1(G, K):
     return 2*(G**3.0/K**2.0)**0.585 - 3
 
 
+def vickers_hardness2(F, a):
+    """
+    Calculate Vickers hardness for a material.
+
+    References:
+        DOI: 10.1007/s10853-013-7569-1 (Title: "Room temperature mechanical properties of natural-mineral-based
+        thermoelectrics")
+
+    Args:
+        F: (float) indentation load (N)
+        a: (float) half of diagonal length of the indentation impression (m)
+
+    Returns: (float) Vickers hardness
+
+    """
+    return (1.8544 * F)/((2*a)**2.0)
+
+
 def thermal_stress(E_T, alpha_T, delta_T, nu_T):
     """
     Calculate thermal stress.
@@ -29,8 +47,8 @@ def thermal_stress(E_T, alpha_T, delta_T, nu_T):
 
     Args:
         E_T: (float) temperature-dependent Young's modulus
-        alpha_T: (float) temperature-dependent thermal expansion coefficient
-        delta_T: (float) temperature difference
+        alpha_T: (float) temperature-dependent thermal expansion coefficient (K^(-1))
+        delta_T: (float) temperature difference (K)
         nu_T: (float) temperature-dependent Poisson's ratio
 
     Returns: (float) thermal stress induced by thermal gradients or thermal transients
