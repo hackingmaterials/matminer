aef6209ae18ddbe2689ce904042e8c8d8699ea41
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-09-14 11:08:00 -0700

    make output depend on the number of results requested per page

--- matminer/data_retrieval/retrieve_Citrine.py
+++ matminer/data_retrieval/retrieve_Citrine.py
@@ -66,9 +66,9 @@ class CitrineDataRetrieval:
             size = len(data.json()['results'])
             start += size
             json_data.append(data.json()['results'])
-            if max_results and len(json_data)*100 > max_results:   # check if limit is reached
-                json_data = json_data[:(max_results / 100)]          # get first multiple of 100 records
-                json_data.append(data.json()['results'][:max_results % 100])    # get remaining records
+            if max_results and len(json_data)*per_page > max_results:   # check if limit is reached
+                json_data = json_data[:(max_results / per_page)]          # get first multiple of 100 records
+                json_data.append(data.json()['results'][:max_results % per_page])    # get remaining records
                 break
             if size < per_page:  # break out of last loop of results
                 break
