741ebcb05f870a9da7e65064cfec274fba4048d5
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-09-17 16:56:17 -0700

    minor docstring addition

--- matminer/data_retrieval/retrieve_Citrine.py
+++ matminer/data_retrieval/retrieve_Citrine.py
@@ -27,6 +27,7 @@ class CitrineDataRetrieval:
     def get_dataframe(self, term=None, formula=None, property=None, contributor=None, reference=None,
                       min_measurement=None, max_measurement=None, from_record=None, data_set_id=None, max_results=None):
         """
+        Gets data from MP in a dataframe format.
         See client docs at http://citrineinformatics.github.io/api-documentation/ for more details on these parameters.
 
         Args:
