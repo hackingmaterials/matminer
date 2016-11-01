a60ed6fc47be5d3617b758f75281ba96fd8960d7
Anubhav Jain <ajain@lbl.gov>
2016-11-01 12:43:04 -0700

    fix issue of child column querying for Mongo data retrieval

--- matminer/data_retrieval/retrieve_MongoDB.py
+++ matminer/data_retrieval/retrieve_MongoDB.py
@@ -18,8 +18,8 @@ class MongoDataRetrieval():
                       limit=None, idx_field=None):
         """
         Args:
-            projection: (list) - a list of fields to grab; dot-notation is allowed.
-                Set to "None" to try to auto-detect the fields
+            projection: (list) - a list of str fields to grab; dot-notation is allowed.
+                Set to "None" to try to auto-detect the fields.
             query: (JSON) - a pymongo-style query to restrict data being gathered
             sort: (tuple) - pymongo-style sort option
             limit: (int) - int to limit the number of entries
@@ -31,8 +31,23 @@ class MongoDataRetrieval():
             d = self.coll.find_one(query, projection, sort=sort)
             projection = d.keys()
 
+        # filter projections - needed because projecting on both a.b and a can cause issues
+        # TODO: add unit test showing why this is needed
+        redundant_projections = []
+        for p in projection:
+            p_list = p.split(".")
+            potential_redundancies = [r for r in projection if
+                                      len(r.split(".")) < len(p.split("."))]
+            for r in potential_redundancies:
+                r_list = r.split(".")
+                if all([p_list[i] == r_list[i] for i in xrange(len(r_list))]):
+                    redundant_projections.append(p)
+
+        mongo_projections = [p for p in projection if p not in
+                             redundant_projections]
+
         all_data = []   # matrix of row, column data
-        r = self.coll.find(query, projection, sort=sort)
+        r = self.coll.find(query, mongo_projections, sort=sort)
         if limit:
             r.limit(limit)
 
