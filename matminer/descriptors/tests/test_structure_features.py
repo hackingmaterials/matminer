139f2b3694271554c740c93a52b0fb2aedec8e56
nisse3000 <n.zimmermann@tuhh.de>
2017-02-09 14:31:24 -0800

    Removed bug in test_structure_features.py.

--- matminer/descriptors/tests/test_structure_features.py
+++ matminer/descriptors/tests/test_structure_features.py
@@ -20,19 +20,25 @@ class StructureFeaturesTest(PymatgenTest):
     def setUp(self):
         self.diamond = Structure(
                 Lattice([[2.189, 0, 1.264], [0.73, 2.064, 1.264],
+                [0, 0, 2.528]]), ["C0+", "C0+"], [[2.554, 1.806, 4.423],
+                [0.365, 0.258, 0.632]], validate_proximity=False,
+                to_unit_cell=False, coords_are_cartesian=True,
+                site_properties=None)
+        self.diamond_no_oxi = Structure(
+                Lattice([[2.189, 0, 1.264], [0.73, 2.064, 1.264],
                 [0, 0, 2.528]]), ["C", "C"], [[2.554, 1.806, 4.423],
                 [0.365, 0.258, 0.632]], validate_proximity=False,
                 to_unit_cell=False, coords_are_cartesian=True,
                 site_properties=None)
         self.nacl = Structure(
                 Lattice([[3.485, 0, 2.012], [1.162, 3.286, 2.012],
-                [0, 0, 4.025]]), ["Na", "Cl"], [[0, 0, 0],
+                [0, 0, 4.025]]), ["Na1+", "Cl1-"], [[0, 0, 0],
                 [2.324, 1.643, 4.025]], validate_proximity=False,
                 to_unit_cell=False, coords_are_cartesian=True,
                 site_properties=None)
         self.cscl = Structure(
                 Lattice([[4.209, 0, 0], [0, 4.209, 0], [0, 0, 4.209]]),
-                ["Cl", "Cs"], [[2.105,2.105, 2.105], [0, 0, 0]],
+                ["Cl1-", "Cs1+"], [[2.105,2.105, 2.105], [0, 0, 0]],
                 validate_proximity=False, to_unit_cell=False,
                 coords_are_cartesian=True, site_properties=None)
 
