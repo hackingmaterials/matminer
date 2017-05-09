6aebf6bb22781c9558aad96693d253eb92fb854a
nisse3000 <n.zimmermann@tuhh.de>
2017-05-08 17:23:22 -0700

    Temporarily removed trigonal bipyramid capabilities.

--- matminer/descriptors/structure_features.py
+++ matminer/descriptors/structure_features.py
@@ -347,7 +347,7 @@ def get_order_parameters(struct, pneighs=None, convert_none_to_zero=True):
         optypes.append("bent")
         opparas.append([float(i), 0.0667])
     for t in ["tet", "oct", "bcc", "q2", "q4", "q6", "reg_tri", "sq", \
-            "sq_pyr", "tri_bipyr"]:
+            "sq_pyr"]: # , "tri_bipyr"]:
         optypes.append(t)
         opparas.append([])
     ops = OrderParameters(optypes, opparas, 100.0)
@@ -392,7 +392,7 @@ def get_order_parameter_stats(
     for i in range(5, 180, 5):
         optypes.append("bent{}".format(i))
     for t in ["tet", "oct", "bcc", "q2", "q4", "q6", "reg_tri", "sq", \
-            "sq_pyr", "tri_bipyr"]:
+            "sq_pyr"]: # , "tri_bipyr"]:
         optypes.append(t)
     opvals = get_order_parameters(
             struct, pneighs=pneighs, convert_none_to_zero=convert_none_to_zero)
@@ -463,9 +463,9 @@ def site_is_of_motif_type(struct, n, pneighs=None, thresh=None):
     if cn == 4 and ops[n][37] > thresh["qtet"]:
         motif_type = "tetrahedral"
         nmotif += 1
-    if cn == 5 and ops[n][46] > thresh["qtribipyr"]:
-        motif_type = "trigonal bipyramidal"
-        nmotif += 1
+    #if cn == 5 and ops[n][46] > thresh["qtribipyr"]:
+    #    motif_type = "trigonal bipyramidal"
+    #    nmotif += 1
     if cn == 6 and ops[n][38] > thresh["qoct"]:
         motif_type = "octahedral"
         nmotif += 1
