64d6bb00a222bc6c50f94f059e7b3a3ae6e6b488
JFChen3 <jc75@rice.edu>
2017-06-30 19:23:20 -0500

    updated unit test

--- matminer/descriptors/tests/test_composition_features.py
+++ matminer/descriptors/tests/test_composition_features.py
@@ -1,36 +1,40 @@
 import unittest2 as unittest
+import pandas as pd
 
-from matminer.descriptors.composition_features import get_stoich_attributes, get_elem_property_attributes, get_valence_orbital_attributes, get_ionic_attributes
+from matminer.descriptors.composition_features_2 import StoichAttributes, ElemPropertyAttributes, ValenceOrbitalAttributes, IonicAttributes 
 from pymatgen.util.testing import PymatgenTest
 
 class CompositionFeaturesTest(PymatgenTest):
 
+    def setUp(self):
+        self.df = pd.DataFrame({"composition":["Fe2O3"]})
+
     def test_stoich(self):
-        self.assertAlmostEqual(get_stoich_attributes("Fe2O3", 0), 2)
-        self.assertAlmostEqual(get_stoich_attributes("Fe2O3", 7), 0.604895199)
+        df_stoich = StoichAttributes().featurize_all(self.df)
+        self.assertAlmostEqual(df_stoich["0-norm"][0], 2)
+        self.assertAlmostEqual(df_stoich["7-norm"][0], 0.604895199)
 
     def test_elem(self):
-        all_attr = get_elem_property_attributes("Fe2O3")
-        atomic_no_attr = all_attr[0]
-        self.assertAlmostEqual(atomic_no_attr[0], 8)
-        self.assertAlmostEqual(atomic_no_attr[1], 26)
-        self.assertAlmostEqual(atomic_no_attr[2], 18)
-        self.assertAlmostEqual(atomic_no_attr[3], 15.2)
-        self.assertAlmostEqual(atomic_no_attr[4], 8.64)
-        self.assertAlmostEqual(atomic_no_attr[5], 8)
+        df_elem = ElemPropertyAttributes().featurize_all(self.df)
+        self.assertAlmostEqual(df_elem["Min Number"][0], 8)
+        self.assertAlmostEqual(df_elem["Max Number"][0], 26)
+        self.assertAlmostEqual(df_elem["Range Number"][0], 18)
+        self.assertAlmostEqual(df_elem["Mean Number"][0], 15.2)
+        self.assertAlmostEqual(df_elem["AbsDev Number"][0], 8.64)
+        self.assertAlmostEqual(df_elem["Mode Number"][0], 8)
 
     def test_valence(self):
-        Fs, Fp, Fd, Ff = get_valence_orbital_attributes("Fe2O3")
-        self.assertAlmostEqual(Fs, 0.294117647)
-        self.assertAlmostEqual(Fp, 0.352941176)
-        self.assertAlmostEqual(Fd, 0.352941176)
-        self.assertAlmostEqual(Ff, 0)
+        df_val = ValenceOrbitalAttributes().featurize_all(self.df)
+        self.assertAlmostEqual(df_val["Frac s Valence Electrons"][0], 0.294117647)
+        self.assertAlmostEqual(df_val["Frac d Valence Electrons"][0], 0.352941176)
+        self.assertAlmostEqual(df_val["Frac p Valence Electrons"][0], 0.352941176)
+        self.assertAlmostEqual(df_val["Frac f Valence Electrons"][0], 0)
 
     def test_ionic(self):
-        cpd_possible, max_ionic_char, avg_ionic_char = get_ionic_attributes("Fe2O3")
-        self.assertEqual(cpd_possible, True)
-        self.assertAlmostEqual(max_ionic_char, 0.476922164)
-        self.assertAlmostEqual(avg_ionic_char, 0.114461319)
+        df_ionic = IonicAttributes().featurize_all(self.df)
+        self.assertEqual(df_ionic["compound possible"][0], 1.0)
+        self.assertAlmostEqual(df_ionic["Max Ionic Char"][0], 0.476922164)
+        self.assertAlmostEqual(df_ionic["Avg Ionic Char"][0], 0.114461319)
 
 if __name__=="__main__":
     unittest.main()
