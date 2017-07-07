a71480709f41550b08320cd27ac5a85453eac9e6
Kiran Mathew <kmathew@lbl.gov>
2017-07-07 14:21:13 -0700

    get rid off add_descriptors.py and move it to a simpler function in the utils.py module

--- matminer/descriptors/utils.py
+++ matminer/descriptors/utils.py
@@ -4,15 +4,16 @@ from functools import reduce
 
 from pymatgen import Composition, MPRester, Element
 
-from matminer.descriptors.data import cohesive_energy_data
+from matminer.descriptors.data import cohesive_energy_data, PymatgenData
 
 
 def get_cohesive_energy(comp):
     """
-    Get cohesive energy of compound by subtracting elemental cohesive energies from the formation energy of the compund.
-    Elemental cohesive energies are taken from http://www.knowledgedoor.com/2/elements_handbook/cohesive_energy.html.
-    Most of them are taken from "Charles Kittel: Introduction to Solid State Physics, 8th edition. Hoboken, NJ:
-    John Wiley & Sons, Inc, 2005, p. 50."
+    Get cohesive energy of compound by subtracting elemental cohesive energies from the formation
+    energy of the compund. Elemental cohesive energies are taken from
+    http://www.knowledgedoor.com/2/elements_handbook/cohesive_energy.html.
+    Most of them are taken from "Charles Kittel: Introduction to Solid State Physics, 8th edition.
+    Hoboken, NJ: John Wiley & Sons, Inc, 2005, p. 50."
 
     Args:
         comp: (str) compound composition, eg: "NaCl"
@@ -42,8 +43,9 @@ def get_cohesive_energy(comp):
 def band_center(comp):
     """
     Estimate absolution position of band center using geometric mean of electronegativity
-    Ref: Butler, M. a. & Ginley, D. S. Prediction of Flatband Potentials at Semiconductor-Electrolyte Interfaces from
-    Atomic Electronegativities. J. Electrochem. Soc. 125, 228 (1978).
+    Ref: Butler, M. a. & Ginley, D. S. Prediction of Flatband Potentials at
+    Semiconductor-Electrolyte Interfaces from Atomic Electronegativities.
+    J. Electrochem. Soc. 125, 228 (1978).
 
     Args:
         comp: (Composition)
@@ -81,3 +83,34 @@ def get_holder_mean(data_lst, power):
         for value in data_lst:
             total += value ** power
         return (total / len(data_lst)) ** (1 / float(power))
+
+
+def add_element_property_column(df, property_name, filter_func, formula_column="formula",
+                                data_source=None):
+    """
+    Add elemental property data filtered by the 'filter_func' for each formula in the given
+    dataframe's 'formula_column' to the given dataframe.
+
+    Args:
+        df (pandas.DataFrame): the dataframe containing the list of formulae to process.
+        property_name (str): elemental property name. Note: must be supported by the given
+            data_source.
+        filter_func (func): the function to apply to the raw elemental property values
+        formula_column (str): the name of the column in the dataframe containing the formulae
+            to process
+        data_source (data.AbstractData): data object that will be used to obtain the elemental
+            properties. eg. MagpieData() or PymatgenData()
+
+    Returns:
+        pandas.DataFrame: modified dataframe with the added elemental property column.
+    """
+    property_column_name = "{}_{}".format(property_name, filter_func.__name__)
+    data_source = data_source or PymatgenData()
+    try:
+        map_func = lambda x: filter_func(data_source.get_property(Composition(x), property_name))
+        df[property_column_name] = df[formula_column].map(map_func)
+    except ValueError:
+        df.loc[property_column_name] = None
+    except AttributeError:
+        raise ValueError('Invalid Elemental property!')
+    return df
