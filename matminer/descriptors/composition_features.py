8da72f514ab52c717d49044b696f9b9c7374096f
JFChen3 <jc75@rice.edu>
2017-07-19 17:58:29 -0500

    debugging

--- matminer/descriptors/composition_features.py
+++ matminer/descriptors/composition_features.py
@@ -223,10 +223,11 @@ class IonicAttributes(BaseFeaturizer):
             print(values_int)
             print(all_elec)
             for i in range(1,len(values_int)+1):
-                print(ind)
                 ind = int(sum(values_int[:i])-1)
+                print(ind)
                 ox_states.append(all_ox_states[ind])
                 elec.append(all_elec[ind])
+                print(all_elec[ind])
 
             #Determine if neutral compound is possible
             cpd_possible = False
