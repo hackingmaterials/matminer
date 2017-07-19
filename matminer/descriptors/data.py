d6836d36b3e91da321f5cc92f9ea8a13dac61535
JFChen3 <jc75@rice.edu>
2017-07-19 15:43:55 -0500

    modified import for python 3 compatibility

--- matminer/descriptors/data.py
+++ matminer/descriptors/data.py
@@ -53,7 +53,7 @@ class DemlData(AbstractData):
     """
     
     def __init__(self):
-        from data_files.deml_elementdata import properties
+        from matminer.descriptors.data_files.deml_elementdata import properties
         self.all_props = properties
         self.available_props = self.all_props.keys() + ["formal_charge","valence_s","valence_p","valence_d","first_ioniz","total_ioniz"]
 
