e748be7c192ddc5c11cda785abea867aede52a73
JFChen3 <jc75@rice.edu>
2017-07-21 15:47:46 -0500

    debugging

--- matminer/descriptors/composition_features.py
+++ matminer/descriptors/composition_features.py
@@ -271,7 +271,7 @@ class IonicAttributes(BaseFeaturizer):
             cpd_possible = False
             ox_sets = itertools.product(*ox_states)
             for ox in ox_sets:
-                print(ox_sets)
+                print(ox)
                 if abs(np.dot(ox, values)) < 1e-4:
                     cpd_possible = True
                     break    
