132e0d765f781b8c37aa9e96f1e3754e8fef7346
Logan Ward <ward.logan.t@gmail.com>
2017-07-27 11:49:08 -0500

    Removed legacy "get_pymatgen/magpie_descriptor"
    These are now replaced with the "matminer/descriptor/data" classes

--- matminer/learners/volume_predictor.py
+++ matminer/learners/volume_predictor.py
@@ -7,7 +7,9 @@ import numpy as np
 from pymatgen.analysis.bond_valence import BVAnalyzer
 from pymatgen.analysis.structure_matcher import StructureMatcher
 
-from matminer.descriptors.composition_features import get_pymatgen_descriptor
+from matminer.descriptors.data import PymatgenData
+
+pmgdata = PymatgenData()
 
 
 class VolumePredictor:
@@ -56,7 +58,7 @@ class VolumePredictor:
 
         smallest_ratio = None  # ratio of observed vs expected bond distance
         ionic_mix = min(
-            np.std(get_pymatgen_descriptor(structure.composition, 'X')) * self.ionic_factor, 1)
+            np.std(pmgdata.get_property(structure.composition, 'X')) * self.ionic_factor, 1)
 
         for site in structure:
             el1 = site.specie
