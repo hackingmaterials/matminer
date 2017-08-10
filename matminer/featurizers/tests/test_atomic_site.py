75ff961e9db1ccf09ac88c1f603ae76fb8d6fc17
Logan Ward <ward.logan.t@gmail.com>
2017-08-10 14:17:03 -0500

    Added class to compute AGNI fingerprints

---
+++ matminer/featurizers/tests/test_atomic_site.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+from pymatgen import Structure, Lattice
+from pymatgen.util.testing import PymatgenTest
+
+from matminer.featurizers.atomic_site import AGNIFingerprints
+
+
+class AGNIFingerprintTests(PymatgenTest):
+    def setUp(self):
+        self.sc = Structure(
+            Lattice([[3.52, 0, 0], [0, 3.52, 0], [0, 0, 3.52]]),
+            ["Al", ],
+            [[0, 0, 0]],
+            validate_proximity=False, to_unit_cell=False,
+            coords_are_cartesian=False)
+        self.cscl = Structure(
+            Lattice([[4.209, 0, 0], [0, 4.209, 0], [0, 0, 4.209]]),
+            ["Cl1-", "Cs1+"], [[0.45, 0.5, 0.5], [0, 0, 0]],
+            validate_proximity=False, to_unit_cell=False,
+            coords_are_cartesian=False)
+
+    def test_simple_cubic(self):
+        """Test with an easy structure"""
+
+        # Make sure direction-dependent fingerprints are zero
+        agni = AGNIFingerprints(directions=['x', 'y', 'z'])
+
+        features = agni.featurize(self.sc, 0)
+        self.assertEquals(8 * 3, len(features))
+        self.assertEquals(8 * 3, len(set(agni.feature_labels())))
+        self.assertArrayAlmostEqual([0, ] * 24, features)
+
+        # Compute the "atomic fingerprints"
+        agni.directions = [None]
+        agni.cutoff = 3.75  # To only get 6 neighbors to deal with
+
+        features = agni.featurize(self.sc, 0)
+        self.assertEquals(8, len(features))
+        self.assertEquals(8, len(set(agni.feature_labels())))
+
+        self.assertEquals(0.8, agni.etas[0])
+        self.assertAlmostEquals(6 * np.exp(-(3.52 / 0.8) ** 2) * 0.5 * (np.cos(np.pi * 3.52 / 3.75) + 1), features[0])
+        self.assertAlmostEquals(6 * np.exp(-(3.52 / 16) ** 2) * 0.5 * (np.cos(np.pi * 3.52 / 3.75) + 1), features[-1])
+
+    def test_off_center_cscl(self):
+        agni = AGNIFingerprints(directions=[None, 'x', 'y', 'z'], cutoff=4)
+
+        # Compute the features on both sites
+        site1 = agni.featurize(self.cscl, 0)
+        site2 = agni.featurize(self.cscl, 1)
+
+        # The atomic attributes should be equal
+        self.assertArrayAlmostEqual(site1[:8], site2[:8])
+
+        # The direction-dependent ones should be equal and opposite in sign
+        self.assertArrayAlmostEqual(-1 * site1[8:], site2[8:])
+
+        # Make sure the site-ones are as expected.
+        right_dist = 4.209 * np.sqrt(0.45 ** 2 + 2 * 0.5 ** 2)
+        right_xdist = 4.209 * 0.45
+        left_dist = 4.209 * np.sqrt(0.55 ** 2 + 2 * 0.5 ** 2)
+        left_xdist = 4.209 * 0.55
+        self.assertAlmostEquals(4 * (
+            right_xdist / right_dist * np.exp(-(right_dist / 0.8) ** 2) * 0.5 * (np.cos(np.pi * right_dist / 4) + 1) -
+            left_xdist / left_dist * np.exp(-(left_dist / 0.8) ** 2) * 0.5 * (np.cos(np.pi * left_dist / 4) + 1)),
+                                site1[8])
+
+    def test_dataframe(self):
+        data = pd.DataFrame({'strc': [self.cscl, self.cscl, self.sc], 'site': [0, 1, 0]})
+
+        agni = AGNIFingerprints()
+        agni.featurize_dataframe(data, ['strc', 'site'])
