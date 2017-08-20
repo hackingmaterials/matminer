78c73de28d754b86f654bbaab30cd2508e05d674
Anubhav Jain <ajain@lbl.gov>
2017-08-19 17:51:33 -0700

    update citrination-client version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         zip_safe=False,
         install_requires=['pymatgen>=2017.7.4', 'tqdm>=4.14.0', 'pandas>=0.20.1',
                           'pymongo>=3.4.0', 'pint>=0.8.1', 'six>=1.10.0'],
-        extras_require={'citrine': ['citrination-client>=1.3.1'],
+        extras_require={'citrine': ['citrination-client>=1.5.3'],
                         'mpds': ['jmespath>=0.9.3', 'ujson>=1.35', 'httplib2>=0.10.3'],
                         'plot': ['matplotlib>=2.0.0', 'plotly>=2.0.12'],
                         'pycookiecheat': ['pycookiecheat>=0.4.0']},
