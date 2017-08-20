f824bdfd3af5f20577f569bf386e3dbb3b247205
Anubhav Jain <ajain@lbl.gov>
2017-08-19 17:32:56 -0700

    cleanups: tab->space (see PEP), remove unneeded function, use OS-safe join

--- matminer/datasets/dataframe_loader.py
+++ matminer/datasets/dataframe_loader.py
@@ -1,16 +1,16 @@
 import pandas
 import os
 
-LOAD_PATH = os.path.dirname(os.path.abspath(__file__))
+module_dir = os.path.dirname(os.path.abspath(__file__))
 
-def load_df(filename):
-	return pandas.read_csv(filename)
 
 def load_elastic_tensor():
-	return load_df(LOAD_PATH+"/ec.csv")
+    return pandas.read_csv(os.path.join(module_dir, "ec.csv"))
+
 
 def load_piezoelectric_tensor():
-	return load_df(LOAD_PATH+"/piezo.csv")
+    return pandas.read_csv(os.path.join(module_dir, "piezo.csv"))
+
 
 def load_dielectric_const_and_ref_ind():
-	return load_df(LOAD_PATH+"/diel_ref.csv")
+    return pandas.read_csv(os.path.join(module_dir, "diel_ref.csv"))
