67febee79fd62f08e592c829cf2f7473eb978962
Anubhav Jain <ajain@lbl.gov>
2017-09-01 16:03:35 -0700

    make BPE a featurizer

--- matminer/featurizers/bandstructure.py
+++ matminer/featurizers/bandstructure.py
@@ -1,69 +1,96 @@
 from __future__ import division, unicode_literals, print_function
 
+from matminer.featurizers.base import BaseFeaturizer
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 __author__ = 'Anubhav Jain <ajain@lbl.gov>'
 
-# TODO: make featurizer
-def branch_point_energy(bs, n_vb=1, n_cb=1):
-    """
-    Get the branch point energy as defined by:
-    Schleife, Fuchs, Rodi, Furthmuller, Bechstedt, APL 94, 012104 (2009)
 
-    Args:
-        bs: (BandStructure) - uniform mesh bandstructure object
-        n_vb: number of valence bands to include
-        n_cb: number of conduction bands to include
+# TODO: add a unit test
 
-    Returns: (int) branch point energy on same energy scale as BS eigenvalues
+class BranchPointEnergy(BaseFeaturizer):
+    def __init__(self, n_vb=1, n_cb=1, calculate_band_edges=True):
+        """
+        Calculates the branch point energy and (optionally) an absolute band
+        edge position assuming the branch point energy is the center of the gap
 
-    """
-    if bs.is_metal():
-        raise ValueError("Cannot define a branch point energy for metals!")
+        Args:
+            n_vb: (int) number of valence bands to include in BPE calc
+            n_cb: (int) number of conduction bands to include in BPE calc
+            calculate_band_edges: (bool) whether to also return band edge
+                positions
+        """
+        self.n_vb = n_vb
+        self.n_cb = n_cb
+        self.calculate_band_edges = calculate_band_edges
 
-    total_sum_energies = 0
-    num_points = 0
-    kpt_wts = SpacegroupAnalyzer(bs.structure).get_kpoint_weights(
-        [k.frac_coords for k in bs.kpoints])
-    for spin in bs.bands:
-        for kpt_idx in range(len(bs.kpoints)):
-            vb_energies = []
-            cb_energies = []
-            for band_idx in range(bs.nb_bands):
-                e = bs.bands[spin][band_idx][kpt_idx]
-                if e > bs.efermi:
-                    cb_energies.append(e)
-                else:
-                    vb_energies.append(e)
-            vb_energies.sort(reverse=True)
-            cb_energies.sort()
-            total_sum_energies += (sum(vb_energies[0:n_vb]) / n_vb +
-                                   sum(cb_energies[0:n_cb]) / n_cb) * kpt_wts[kpt_idx] / 2
+    def featurize(self, bs, target_gap=None):
+        """
+        Args:
+            bs: (BandStructure)
 
-            num_points += kpt_wts[kpt_idx]
+        Returns:
+            (int) branch point energy on same energy scale as BS eigenvalues
+        """
+        if bs.is_metal():
+            raise ValueError("Cannot define a branch point energy for metals!")
 
-    return total_sum_energies / num_points
+        total_sum_energies = 0
+        num_points = 0
+        kpt_wts = SpacegroupAnalyzer(bs.structure).get_kpoint_weights(
+            [k.frac_coords for k in bs.kpoints])
+        for spin in bs.bands:
+            for kpt_idx in range(len(bs.kpoints)):
+                vb_energies = []
+                cb_energies = []
+                for band_idx in range(bs.nb_bands):
+                    e = bs.bands[spin][band_idx][kpt_idx]
+                    if e > bs.efermi:
+                        cb_energies.append(e)
+                    else:
+                        vb_energies.append(e)
+                vb_energies.sort(reverse=True)
+                cb_energies.sort()
+                total_sum_energies += (sum(
+                    vb_energies[0:self.n_vb]) / self.n_vb + sum(
+                    cb_energies[0:self.n_cb]) / self.n_cb) * kpt_wts[
+                                          kpt_idx] / 2
 
-# TODO: make featurizer
-def absolute_band_positions_bpe(bs, target_gap=None, **kwargs):
-    """
-    Absolute VBM and CBM positions with respect to branch point energy
+                num_points += kpt_wts[kpt_idx]
 
-    Args:
-        bs: Bandstructure object
-        target_gap: if a better band gap is known, shift band positions by this gap
-        **kwargs: arguments to feed into branch point energy code
+        bpe = total_sum_energies / num_points
 
-    Returns:
-        (vbm, cbm) - tuple of floats
+        if not self.calculate_band_edges:
+            return [bpe]
 
-    """
-    vbm = bs.get_vbm()["energy"]
-    cbm = bs.get_cbm()["energy"]
-    shift = 0
-    if target_gap:
-        # for now, equal shift to VBM / CBM
-        shift = (target_gap - (cbm - vbm)) / 2
+        vbm = bs.get_vbm()["energy"]
+        cbm = bs.get_cbm()["energy"]
+        shift = 0
+        if target_gap:
+            # for now, equal shift to VBM / CBM
+            shift = (target_gap - (cbm - vbm)) / 2
 
-    bpe = branch_point_energy(bs, **kwargs)
-    return (vbm - bpe - shift), (cbm - bpe + shift)
+        return [bpe, (vbm - bpe - shift), (cbm - bpe + shift)]
+
+    def feature_labels(self):
+
+        return ["branch point energy", "vbm_absolute",
+                "cbm_absolute"] if self.calculate_band_edges else [
+            "branch point energy"]
+
+    def citations(self):
+        return ["""@article{Schleife2009,
+author = {Schleife, A. and Fuchs, F. and R{\"{o}}dl, C. and Furthm{\"{u}}ller, J. and Bechstedt, F.},
+doi = {10.1063/1.3059569},
+isbn = {0003-6951},
+issn = {00036951},
+journal = {Applied Physics Letters},
+number = {1},
+pages = {2009--2011},
+title = {{Branch-point energies and band discontinuities of III-nitrides and III-/II-oxides from quasiparticle band-structure calculations}},
+volume = {94},
+year = {2009}
+}"""]
+
+    def implementors(self):
+        return ["Anubhav Jain"]
