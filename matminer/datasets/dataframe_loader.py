c24f193e8f18b8f0471ac75ea5726ef18912dbb1
Anubhav Jain <ajain@lbl.gov>
2017-09-01 11:16:48 -0700

    fix CSV (due to reverting git-lfs stuff...), add tests, add references to datasets

--- matminer/datasets/dataframe_loader.py
+++ matminer/datasets/dataframe_loader.py
@@ -1,16 +1,34 @@
 import pandas
 import os
 
+
+__author__ = "Kyle Bystrom <kylebystrom@berkeley.edu>, " \
+             "Anubhav Jain <ajain@lbl.gov>"
+
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def load_elastic_tensor():
-    return pandas.read_csv(os.path.join(module_dir, "ec.csv"))
+    # ref: Jong, M. De, Chen, W., Angsten, T., Jain, A., Notestine, R., Gamst,
+    # A., Sluiter, M., Ande, C. K., Zwaag, S. Van Der, Plata, J. J., Toher,
+    # C., Curtarolo, S., Ceder, G., Persson, K. a & Asta, M. Charting the
+    # complete elastic properties of inorganic crystalline compounds. Sci.
+    # Data 2, 150009 (2015).
+    return pandas.read_csv(os.path.join(module_dir, "elastic_tensor.csv"),
+                           comment="#")
 
 
 def load_piezoelectric_tensor():
-    return pandas.read_csv(os.path.join(module_dir, "piezo.csv"))
+    # ref: de Jong, M., Chen, W., Geerlings, H., Asta, M. & Persson, K. A.
+    # A database to enable discovery and design of piezoelectric materials.
+    # Sci. Data 2, 150053 (2015).
+    return pandas.read_csv(os.path.join(module_dir, "piezo.csv"), comment="#")
 
 
-def load_dielectric_const_and_ref_ind():
-    return pandas.read_csv(os.path.join(module_dir, "diel_ref.csv"))
+def load_dielectric_constant():
+    # ref: Petousis, I., Mrdjenovich, D., Ballouz, E., Liu, M., Winston, D.,
+    # Chen, W., Graf, T., Schladt, T. D., Persson, K. A. & Prinz, F. B.
+    # High-throughput screening of inorganic compounds for the discovery of
+    # novel dielectric and optical materials. Sci. Data 4, 160134 (2017).
+    return pandas.read_csv(os.path.join(module_dir, "dielectric_constant.csv"),
+                           comment="#")
