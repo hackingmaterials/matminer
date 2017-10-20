84ca13ac837fddef832b61e60cd3292846c81793
sbajaj <saurabhbajaj2@gmail.com>
2017-10-20 16:24:47 -0700

    minor refactor

--- matminer/data_retrieval/tests/test_retrieve_Citrine.py
+++ matminer/data_retrieval/tests/test_retrieve_Citrine.py
@@ -1,18 +1,14 @@
 # coding: utf-8
 
 from __future__ import division, unicode_literals, absolute_import
-
 import os
-
 import unittest
-
 from matminer.data_retrieval.retrieve_Citrine import CitrineDataRetrieval
 
 citrine_key = os.environ.get('CITRINE_KEY', None)
 
 
-@unittest.skipIf(citrine_key is None,
-                 "CITRINE_KEY environment variable not set.")
+@unittest.skipIf(citrine_key is None, "CITRINE_KEY environment variable not set.")
 class CitrineDataRetrievalTest(unittest.TestCase):
     def setUp(self):
         self.cdr = CitrineDataRetrieval(citrine_key)
@@ -20,7 +16,6 @@ class CitrineDataRetrievalTest(unittest.TestCase):
     def test_get_data(self):
         pifs_lst = self.cdr.get_api_data(formula="W", data_type='EXPERIMENTAL', max_results=10)
         df = self.cdr.get_dataframe(pifs_lst)
-        print df
 
 
 if __name__ == "__main__":
