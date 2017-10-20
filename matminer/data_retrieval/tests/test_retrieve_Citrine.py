1d5627608cbc6cf5ace129bad96973e50771182a
sbajaj <saurabhbajaj2@gmail.com>
2017-10-20 09:51:10 -0700

    add new queries (in old format)

--- matminer/data_retrieval/tests/test_retrieve_Citrine.py
+++ matminer/data_retrieval/tests/test_retrieve_Citrine.py
@@ -20,6 +20,7 @@ class CitrineDataRetrievalTest(unittest.TestCase):
     def test_get_data(self):
         pifs_lst = self.cdr.get_api_data(formula="W", data_type='EXPERIMENTAL', max_results=10)
         df = self.cdr.get_dataframe(pifs_lst)
+        print df
 
 
 if __name__ == "__main__":
