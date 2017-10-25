dfe18c50bdc7215be0b2fd10e2fc641c64d26ffa
sbajaj <saurabhbajaj2@gmail.com>
2017-10-25 15:08:52 -0700

    add assert for checking number of rows

--- matminer/data_retrieval/tests/test_retrieve_Citrine.py
+++ matminer/data_retrieval/tests/test_retrieve_Citrine.py
@@ -21,6 +21,7 @@ class CitrineDataRetrievalTest(unittest.TestCase):
     def test_get_data(self):
         pifs_lst = self.cdr.get_api_data(formula="W", data_type='EXPERIMENTAL', max_results=10)
         df = self.cdr.get_dataframe(pifs_lst)
+        assert df.shape[0] == 10
 
 
 if __name__ == "__main__":
