70c5339fc0d26ca82dd0b457f7ef02842553e576
Anubhav Jain <ajain@lbl.gov>
2017-12-06 11:48:24 -0800

    remove unneeded getter/setter, this is Python

--- matminer/featurizers/site.py
+++ matminer/featurizers/site.py
@@ -23,42 +23,42 @@ from matminer.featurizers.stats import PropertyStats
 
 
 class AGNIFingerprints(BaseFeaturizer):
-    """Integral of the product of the radial distribution function and a Gaussian window function.
-    Originally used by [Botu *et al*](http://pubs.acs.org/doi/abs/10.1021/acs.jpcc.6b10908) to fit empiricial potentials,
-    these features come in two forms: atomic fingerprints, and direction-resolved fingerprints.
-    Atomic fingerprints describe the local environment of an atom and are computed using the function:
+    """Integral of the product of the radial distribution function and a
+    Gaussian window function. Originally used by [Botu *et al*]
+    (http://pubs.acs.org/doi/abs/10.1021/acs.jpcc.6b10908) to fit empiricial
+    potentials. These features come in two forms: atomic fingerprints and
+    direction-resolved fingerprints.
+    Atomic fingerprints describe the local environment of an atom and are
+    computed using the function:
     :math:`A_i(\eta) = \sum\limits_{i \ne j} e^{-(\frac{r_{ij}}{\eta})^2} f(r_{ij})`
     where :math:`i` is the index of the atom, :math:`j` is the index of a neighboring atom, :math:`\eta` is a scaling function,
     :math:`r_{ij}` is the distance between atoms :math:`i` and :math:`j`, and :math:`f(r)` is a cutoff function where
     :math:`f(r) = 0.5[cos(\frac{\pi r_{ij}}{R_c}) + 1]` if :math:`r < R_c:math:` and 0 otherwise.
+
     The direction-resolved fingerprints are computed using
     :math:`V_i^k(\eta) = \sum\limits_{i \ne j} \frac{r_{ij}^k}{r_{ij}} e^{-(\frac{r_{ij}}{\eta})^2} f(r_{ij})`
     where :math:`r_{ij}^k` is the :math:`k^{th}` component of :math:`\bold{r}_i - \bold{r}_j`.
+
     Parameters:
-            directions (iterable): List of directions for the fingerprints. Can be `none`, 'x', 'y', or 'z'
-            etas (iterable of floats): List of which window widths to compute
-            cutoff (float): Cutoff distance
+
     TODO: Differentiate between different atom types (maybe as another class)
     """
 
     def __init__(self, directions=(None, 'x', 'y', 'z'), etas=None,
                  cutoff=8):
+        """
+
+        Args:
+            directions (iterable): List of directions for the fingerprints. Can
+                be one or more of 'None`, 'x', 'y', or 'z'
+            etas (iterable of floats): List of which window widths to compute
+            cutoff (float): Cutoff distance (Angstroms)
+        """
         self.directions = directions
         self.etas = etas
         if self.etas is None:
             self.etas = np.logspace(np.log10(0.8), np.log10(16), 8)
         self.cutoff = cutoff
-
-    @property
-    def directions(self):
-        return self.__directions
-
-    @directions.setter
-    def directions(self, dirs):
-        for d in dirs:
-            if d not in [None, 'x', 'y', 'z']:
-                raise Exception('Direction not `None`, x, y, or z: z')
-        self.__directions = dirs
 
     def featurize(self, strc, site):
         # Get all neighbors of this site
