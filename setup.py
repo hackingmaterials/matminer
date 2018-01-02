a40ecd39677e8aab2015fc1bf7df0ba3e511f420
Logan Ward <ward.logan.t@gmail.com>
2018-01-02 11:37:58 -0600

    Updated pymatgen version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         packages=find_packages(),
         package_data={},
         zip_safe=False,
-        install_requires=['pymatgen>=2017.10.16', 'tqdm>=4.14.0', 'pandas>=0.20.1',
+        install_requires=['pymatgen>=2017.12.30', 'tqdm>=4.14.0', 'pandas>=0.20.1',
                           'pymongo>=3.4.0', 'pint>=0.8.1', 'six>=1.10.0',
                           'citrination-client>=2.1.0', 'plotly>=2.0.12'],
         extras_require={'mpds': ['jmespath>=0.9.3', 'ujson>=1.35', 'httplib2>=0.10.3', 'ase>=3.14.1'],
