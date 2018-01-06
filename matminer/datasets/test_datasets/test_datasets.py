e2b38b9ecd3ba3c2df1f99d4ebba483fa8dc44cf
kylebystrom <kylebystrom@berkeley.edu>
2018-01-05 18:29:27 -0800

    improve test dataset format and tests

--- matminer/datasets/test_datasets/test_datasets.py
+++ matminer/datasets/test_datasets/test_datasets.py
@@ -1,4 +1,6 @@
 import unittest
+import numpy as np
+from pymatgen.core.structure import Structure
 
 from matminer.datasets.dataframe_loader import load_elastic_tensor, \
     load_piezoelectric_tensor, load_dielectric_constant
@@ -7,6 +9,9 @@ from matminer.datasets.dataframe_loader import load_elastic_tensor, \
 class DataSetTest(unittest.TestCase):
     def test_elastic_tensor(self):
         df = load_elastic_tensor()
+        self.assertEqual(type(df['structure'][0]), Structure)
+        for c in ['compliance_tensor', 'elastic_tensor', 'elastic_tensor_original']:
+            self.assertEqual(type(df[c][0]), np.ndarray)
         self.assertEqual(len(df), 1181)
         column_headers = {'idx', 'G_Reuss', 'G_VRH', 'G_Voigt', 'K_Reuss',
                           'K_VRH', 'K_Voigt', 'compliance_tensor',
@@ -14,23 +19,27 @@ class DataSetTest(unittest.TestCase):
                           'elastic_tensor_original', 'formula',
                           'kpoint_density', 'material_id', 'nsites',
                           'poisson_ratio', 'poscar', 'space_group', 'structure',
-                          'volume'}
+                          'volume', 'cif'}
         self.assertEqual(set(list(df)), column_headers)
 
     def test_piezoelectric_tensor(self):
         df = load_piezoelectric_tensor()
         self.assertEqual(len(df), 941)
+        self.assertEqual(type(df['piezoelectric_tensor'][0]), np.ndarray)
+        self.assertEqual(type(df['structure'][0]), Structure)
         column_headers = {'idx', 'eij_max', 'meta', 'piezoelectric_tensor',
                           'v_max', 'structure', 'poscar', 'formula', 'volume',
-                          'space_group', 'point_group', 'nsites', 'material_id'}
+                          'space_group', 'point_group', 'nsites', 'material_id',
+                          'cif'}
         self.assertEqual(set(list(df)), column_headers)
 
     def test_dielectric_tensor(self):
         df = load_dielectric_constant()
+        self.assertEqual(type(df['structure'][0]), Structure)
         self.assertEqual(len(df), 1056)
         print(list(df))
         column_headers = {'idx', 'band_gap', 'e_electronic', 'e_total', 'meta',
-                          'n', 'poly_electronic', 'poly_total',
+                          'n', 'poly_electronic', 'poly_total', 'cif',
                           'pot_ferroelectric', 'nsites', 'structure', 'poscar',
                           'space_group', 'material_id', 'formula', 'volume'}
         self.assertEqual(set(list(df)), column_headers)
