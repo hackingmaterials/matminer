661ce4c8c8cf903c66fe03bcad60a4738ddcc0dd
Logan Ward <ward.logan.t@gmail.com>
2018-01-12 18:41:39 -0600

    Fix: Make sure DataFrame indices are copied over
    Fixes a problem with `featurize_dataframe`
    
    The current implementation leads to features not being matched
    to the proper entry because the indicies of the dataframe
    are not passed to the new series containing each feature.
    It only works when the first entry is index 0, the second is
    index 1, and so on.
    
    I added a test case that fails without the fix, and added
    the indices of the dataframe to the new Series being generated

--- matminer/featurizers/tests/test_base.py
+++ matminer/featurizers/tests/test_base.py
@@ -39,8 +39,11 @@ class TestBaseClass(PymatgenTest):
         self.multi = MultipleFeaturizer()
         self.matrix = MatrixFeaturizer()
 
+    def make_test_data(self):
+        return pd.DataFrame({'x': [1, 2, 3]})
+
     def test_dataframe(self):
-        data = pd.DataFrame({'x': [1, 2, 3]})
+        data = self.make_test_data()
         data = self.single.featurize_dataframe(data, 'x')
         self.assertArrayAlmostEqual(data['y'], [2, 3, 4])
 
@@ -48,20 +51,28 @@ class TestBaseClass(PymatgenTest):
         self.assertArrayAlmostEqual(data['y'], [0, 1, 2])
         self.assertArrayAlmostEqual(data['z'], [3, 4, 5])
 
+
     def test_matrix(self):
         """Test the ability to add features that are matrices to a dataframe"""
-        data = pd.DataFrame({'x': [1, 2, 3]})
+        data = self.make_test_data()
         data = self.matrix.featurize_dataframe(data, 'x')
         self.assertArrayAlmostEqual(np.eye(2, 2), data['representation'][0])
 
     def test_inplace(self):
-        data = pd.DataFrame({'x': [1, 2, 3]})
+        data = self.make_test_data()
 
         self.single.featurize_dataframe(data, 'x', inplace=False)
         self.assertNotIn('y', data.columns)
 
         self.single.featurize_dataframe(data, 'x', inplace=True)
         self.assertIn('y', data)
+
+    def test_indices(self):
+        data = self.make_test_data()
+        data.index = [4, 6, 6]
+
+        data = self.single.featurize_dataframe(data, 'x')
+        self.assertArrayAlmostEqual(data['y'], [2, 3, 4])
 
 
 if __name__ == '__main__':
