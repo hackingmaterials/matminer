ab23db0c7a049ccd0cb34dee67d03e5405469df7
kylebystrom <kylebystrom@berkeley.edu>
2018-01-19 16:14:03 -0800

    updated sample dataframes and unit tests

--- matminer/datasets/dataframe_loader.py
+++ matminer/datasets/dataframe_loader.py
@@ -11,7 +11,7 @@ __author__ = "Kyle Bystrom <kylebystrom@berkeley.edu>, " \
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def load_elastic_tensor():
+def load_elastic_tensor(include_metadata = False):
     # ref: Jong, M. De, Chen, W., Angsten, T., Jain, A., Notestine, R., Gamst,
     # A., Sluiter, M., Ande, C. K., Zwaag, S. Van Der, Plata, J. J., Toher,
     # C., Curtarolo, S., Ceder, G., Persson, K. a & Asta, M. Charting the
@@ -24,10 +24,16 @@ def load_elastic_tensor():
             df.at[(i,c)] = np.array(ast.literal_eval(df.at[(i,c)]))
     df['cif'] = df['structure']
     df['structure'] = pandas.Series([Poscar.from_string(s).structure for s in df['poscar']])
-    return df
+    new_columns = ['material_id', 'formula',
+        'nsites', 'space_group', 'volume',
+        'structure', 'elastic_anisotropy', 'G_Reuss', 'G_VRH', 'G_Voigt', 'K_Reuss',
+        'K_VRH', 'K_Voigt', 'poisson_ratio',
+        'compliance_tensor', 'elastic_tensor', 'elastic_tensor_original']
+    if include_metadata:
+        new_columns += ['cif', 'kpoint_density', 'poscar']
+    return df[new_columns]
 
-
-def load_piezoelectric_tensor():
+def load_piezoelectric_tensor(include_metadata = False):
     # ref: de Jong, M., Chen, W., Geerlings, H., Asta, M. & Persson, K. A.
     # A database to enable discovery and design of piezoelectric materials.
     # Sci. Data 2, 150053 (2015).
@@ -37,10 +43,14 @@ def load_piezoelectric_tensor():
         df.at[(i,c)] = np.array(ast.literal_eval(df.at[(i,c)]))
     df['cif'] = df['structure']
     df['structure'] = pandas.Series([Poscar.from_string(s).structure for s in df['poscar']])
-    return df
+    new_columns = ['material_id', 'formula',
+        'nsites', 'point_group', 'space_group', 'volume',
+        'structure', 'eij_max', 'v_max', 'piezoelectric_tensor']
+    if include_metadata:
+        new_columns += ['cif', 'meta', 'poscar']
+    return df[new_columns]
 
-
-def load_dielectric_constant():
+def load_dielectric_constant(include_metadata = False):
     # ref: Petousis, I., Mrdjenovich, D., Ballouz, E., Liu, M., Winston, D.,
     # Chen, W., Graf, T., Schladt, T. D., Persson, K. A. & Prinz, F. B.
     # High-throughput screening of inorganic compounds for the discovery of
@@ -49,4 +59,11 @@ def load_dielectric_constant():
                            comment="#")
     df['cif'] = df['structure']
     df['structure'] = pandas.Series([Poscar.from_string(s).structure for s in df['poscar']])
-    return df
+    new_columns = ['material_id', 'formula',
+        'nsites', 'space_group', 'volume',
+        'structure',
+        'band_gap', 'e_electronic', 'e_total', 'n', 'poly_electronic',
+        'poly_total', 'pot_ferroelectric']
+    if include_metadata:
+        new_columns += ['cif', 'meta', 'poscar']
+    return df[new_columns]
--- matminer/datasets/test_datasets/test_datasets.py
+++ matminer/datasets/test_datasets/test_datasets.py
@@ -13,36 +13,46 @@ class DataSetTest(unittest.TestCase):
         for c in ['compliance_tensor', 'elastic_tensor', 'elastic_tensor_original']:
             self.assertEqual(type(df[c][0]), np.ndarray)
         self.assertEqual(len(df), 1181)
-        column_headers = {'idx', 'G_Reuss', 'G_VRH', 'G_Voigt', 'K_Reuss',
-                          'K_VRH', 'K_Voigt', 'compliance_tensor',
-                          'elastic_anisotropy', 'elastic_tensor',
-                          'elastic_tensor_original', 'formula',
-                          'kpoint_density', 'material_id', 'nsites',
-                          'poisson_ratio', 'poscar', 'space_group', 'structure',
-                          'volume', 'cif'}
-        self.assertEqual(set(list(df)), column_headers)
+        column_headers = ['material_id', 'formula',
+                          'nsites', 'space_group', 'volume',
+                          'structure', 'elastic_anisotropy', 'G_Reuss',
+                          'G_VRH', 'G_Voigt', 'K_Reuss',
+                          'K_VRH', 'K_Voigt', 'poisson_ratio',
+                          'compliance_tensor', 'elastic_tensor',
+                          'elastic_tensor_original']
+        self.assertEqual(list(df), column_headers)
+        df = load_elastic_tensor(include_metadata = True)
+        column_headers += ['cif', 'kpoint_density', 'poscar']
+        self.assertEqual(list(df), column_headers)
 
     def test_piezoelectric_tensor(self):
         df = load_piezoelectric_tensor()
         self.assertEqual(len(df), 941)
         self.assertEqual(type(df['piezoelectric_tensor'][0]), np.ndarray)
         self.assertEqual(type(df['structure'][0]), Structure)
-        column_headers = {'idx', 'eij_max', 'meta', 'piezoelectric_tensor',
-                          'v_max', 'structure', 'poscar', 'formula', 'volume',
-                          'space_group', 'point_group', 'nsites', 'material_id',
-                          'cif'}
-        self.assertEqual(set(list(df)), column_headers)
+        column_headers = ['material_id', 'formula',
+                          'nsites', 'point_group', 'space_group', 'volume',
+                          'structure', 'eij_max', 'v_max', 'piezoelectric_tensor']
+        self.assertEqual(list(df), column_headers)
+        df = load_piezoelectric_tensor(include_metadata = True)
+        column_headers += ['cif', 'meta', 'poscar']
+        self.assertEqual(list(df), column_headers)
 
     def test_dielectric_tensor(self):
         df = load_dielectric_constant()
         self.assertEqual(type(df['structure'][0]), Structure)
         self.assertEqual(len(df), 1056)
         print(list(df))
-        column_headers = {'idx', 'band_gap', 'e_electronic', 'e_total', 'meta',
-                          'n', 'poly_electronic', 'poly_total', 'cif',
-                          'pot_ferroelectric', 'nsites', 'structure', 'poscar',
-                          'space_group', 'material_id', 'formula', 'volume'}
-        self.assertEqual(set(list(df)), column_headers)
+        column_headers = ['material_id', 'formula',
+                          'nsites', 'space_group', 'volume',
+                          'structure',
+                          'band_gap', 'e_electronic', 'e_total',
+                          'n', 'poly_electronic',
+                          'poly_total', 'pot_ferroelectric']
+        self.assertEqual(list(df), column_headers)
+        df = load_dielectric_constant(include_metadata = True)
+        column_headers += ['cif', 'meta', 'poscar']
+        self.assertEqual(list(df), column_headers)
 
 
 if __name__ == "__main__":
