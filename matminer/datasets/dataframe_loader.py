2fe0b0239126b456937becbf296652c10e7b3c1b
kylebystrom <kylebystrom@berkeley.edu>
2018-01-19 16:39:23 -0800

    material_id as index for test datasets

--- matminer/datasets/dataframe_loader.py
+++ matminer/datasets/dataframe_loader.py
@@ -18,8 +18,9 @@ def load_elastic_tensor(include_metadata = False):
     # complete elastic properties of inorganic crystalline compounds. Sci.
     # Data 2, 150009 (2015).
     df = pandas.read_csv(os.path.join(module_dir, "elastic_tensor.csv"),
-                           comment="#")
-    for i in range(df.shape[0]):
+        comment="#")
+    df.set_index('material_id')
+    for i in list(df.index):
         for c in ['compliance_tensor', 'elastic_tensor', 'elastic_tensor_original']:
             df.at[(i,c)] = np.array(ast.literal_eval(df.at[(i,c)]))
     df['cif'] = df['structure']
@@ -37,8 +38,10 @@ def load_piezoelectric_tensor(include_metadata = False):
     # ref: de Jong, M., Chen, W., Geerlings, H., Asta, M. & Persson, K. A.
     # A database to enable discovery and design of piezoelectric materials.
     # Sci. Data 2, 150053 (2015).
-    df = pandas.read_csv(os.path.join(module_dir, "piezoelectric_tensor.csv"), comment="#")
-    for i in range(df.shape[0]):
+    df = pandas.read_csv(os.path.join(module_dir, "piezoelectric_tensor.csv"),
+        comment="#")
+    df.set_index('material_id')
+    for i in list(df.index):
         c = 'piezoelectric_tensor'
         df.at[(i,c)] = np.array(ast.literal_eval(df.at[(i,c)]))
     df['cif'] = df['structure']
@@ -56,7 +59,8 @@ def load_dielectric_constant(include_metadata = False):
     # High-throughput screening of inorganic compounds for the discovery of
     # novel dielectric and optical materials. Sci. Data 4, 160134 (2017).
     df = pandas.read_csv(os.path.join(module_dir, "dielectric_constant.csv"),
-                           comment="#")
+        comment="#")
+    df.set_index('material_id')
     df['cif'] = df['structure']
     df['structure'] = pandas.Series([Poscar.from_string(s).structure for s in df['poscar']])
     new_columns = ['material_id', 'formula',
