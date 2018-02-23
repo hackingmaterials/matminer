85299b24c2536e9c5e51b7709e4b906ac3246299
Maxwell Dylla <280mtd@gmail.com>
2018-02-22 19:59:01 -0600

    moved grdf and afs to site, implemented fit method for grdf, updated tests

--- matminer/featurizers/site.py
+++ matminer/featurizers/site.py
@@ -1,13 +1,17 @@
 from __future__ import division
 
-"""Features that describe the local environment of a single atom
+"""
+Features that describe the local environment of a single atom. Note that
+structural featues can be constructed from a combination of site features from
+every stite in the structure.
+
 The `featurize` function takes two arguments:
-    struct (Structure): Object representing the structure containing the site 
+    struct (Structure): Object representing the structure containing the site
         of interest
     idx (int): Index of the site to be featurized
-We have to use two parameters because the Site object does not hold a pointer 
+We have to use two parameters because the Site object does not hold a pointer
 back to its structure and often information on neighbors is required. To run
-:code:`featurize_dataframe`, you must pass the column names for both the site 
+:code:`featurize_dataframe`, you must pass the column names for both the site
 index and the structure. For example:
 .. code:: python
     f = AGNIFingerprints()
@@ -16,12 +20,15 @@ index and the structure. For example:
 
 import os
 import ruamel.yaml as yaml
+import itertools
 import numpy as np
 import math
+import scipy.integrate as integrate
 
 from collections import defaultdict
 
 from matminer.featurizers.base import BaseFeaturizer
+from math import pi
 from scipy.spatial import Voronoi, Delaunay
 from pymatgen.analysis.local_env import LocalStructOrderParas, \
     VoronoiNN, JMolNN, MinimumDistanceNN, MinimumOKeeffeNN, \
@@ -1307,3 +1314,247 @@ class CoordinationNumber(BaseFeaturizer):
 
     def implementors(self):
         return ['Nils E. R. Zimmermann']
+
+
+class GeneralizedRadialDistributionFunction(BaseFeaturizer):
+    """
+    Compute the general radial distribution function (GRDF) for a site. The
+    GRDF is a radial measure of crystal order around a site. There are two
+    featurizing modes:
+
+    1. GRDF: (reccomended) - n_bins length vector
+        In GRDF mode, The GRDF is computed by considering all sites around a
+        central site (i.e., no sites are ommited when computing the GRDF). The
+        features output from this mode will be vectors with length n_bins.
+
+    2. pairwise GRDF: (advanced users) - n_bins x n_sites matrix
+        In this mode, GRDFs are are still computed around a central site, but
+        only one other site (and their translational eqivalants) are used to
+        compute a GRDF (e.g. site 1 with site 2 and the translational
+        equivalants of site 2). This results in a a n_sites x n_bins matix of
+        features. Requires `fit` for determining the max number of sites for
+
+    The GRDF is a generalization of the partial radial distribution function
+    (PRDF). In contrast with the PRDF, the bins of the GRDF are not mutually-
+    exclusive and need not carry a constant weight of 1. The PRDF is a case of
+    the GRDF when the bins are rectangular functions. Examples of other
+    functions to use with the GRDF are gaussians, trig, and bessel functions.
+
+    Args:
+        bins:   (list of touples) a list of (str, functions). The str is a text
+                    label for each bin functional. The functions should accept
+                    scalar numpy arrays (each scalar value corresponds to a
+                    distance) and return arrays of floats.
+                    (e.g. lambda d: exp( a_0 * (d - b_0)**2 ))
+        cutoff: (float) maximum distance to look for neighbors
+        mode:   (str) the featurizing mode. supported options are:
+                    'GRDF' and 'pairwise_GRDF'
+    """
+
+    def __init__(self, bins, cutoff=20.0, mode='GRDF'):
+        self.bins = bins
+        self.cutoff = cutoff
+
+        if mode not in ['GRDF', 'pairwise_GRDF']:
+            raise AttributeError('{} is not a valid GRDF mode. try '
+                                 '"GRDF" or "pairwise_GRDF"'.format(mode))
+        else:
+            self.mode = mode
+
+        self.fit_labels = None
+
+    def fit(self, X, y=None, **fit_kwargs):
+        """
+        Determine the maximum number of sites in X to assign correct feature
+        labels
+
+        Args:
+            X - [list of tuples], training data
+                tuple values should be (struc, idx)
+        """
+
+        max_sites = max([len(X[i][0]._sites) for i in range(len(X))])
+        self.fit_labels = ['site2 {} {}'.format(i, bin[0]) for bin in self.bins
+                           for i in range(max_sites)]
+
+    def featurize(self, struct, idx):
+        """
+        Get GRDF of the input structure.
+        Args:
+            struct (Structure): Pymatgen Structure object.
+            idx (int): index of target site in structure struct.
+
+        Returns:
+            Flattened list of GRDF values. For each run mode the list order is:
+                GRDF:          bin#
+                pairwise GRDF: site2# bin#
+            The site2# corresponds to a pymatgen site index and bin#
+            corresponds to one of the bin functionals
+        """
+
+        if not struct.is_ordered:
+            raise ValueError("Disordered structure support not built yet")
+
+        # Get list of neighbors by site
+        # Idexing is [site#][neighbor#][pymatgen Site, distance, site index]
+        sites = struct._sites
+        central_site = sites[idx]
+        neighbors_lst = struct.get_neighbors(central_site, self.cutoff,
+                                             include_index=True)
+        sites = range(0, len(sites))
+
+        # Generate lists of pairwise distances according to run mode
+        if self.mode == 'GRDF':
+            # Make a single distance collection
+            distance_collection = [
+                [neighbor[1] for neighbor in neighbors_lst]]
+        else:
+            # Make pairwise distance collections for pairwise GRDF
+            distance_collection = [
+                [neighbor[1] for neighbor in neighbors_lst
+                    if neighbor[2] == site_idx] for site_idx in sites]
+
+        # compute bin counts for each list of pairwise distances
+        bin_counts = []
+        for values in distance_collection:
+            values = np.array(values)  # use scalar array function calling
+            bin_counts.append([sum(bin[1](values)) for bin in self.bins])
+
+        # Compute "volume" of each bin to normalize GRDFs
+        integrations = [integrate.quad(lambda x: 4. * pi * bin[1](x) * x**2.,
+                                       0, self.cutoff) for bin in self.bins]
+
+        volumes = [item[0] for item in integrations]
+        errors = [item[1] for item in integrations]
+        if max(errors) > 1e-5:
+            raise ValueError('Numerical integration does not play well with '
+                             'the chosen bin functionals, choose new ones.')
+
+        # normalize the bin counts by the bin volume to compute features
+        features = []
+        for values in bin_counts:
+            features.extend(np.array(values) / np.array(volumes))
+
+        return features
+
+    def feature_labels(self):
+        if self.mode == 'GRDF':
+            return [bin[0] for bin in self.bins]
+        else:
+            if self.fit_labels:
+                return self.fit_labels
+            else:
+                raise AttributeError('the fit method must be called first, to '
+                                     'determine the correct feature labels.')
+
+    def citations(self):
+        return ['@article{PhysRevB.95.144110, title = {Representation of compo'
+                'unds for machine-learning prediction of physical properties},'
+                ' author = {Seko, Atsuto and Hayashi, Hiroyuki and Nakayama, '
+                'Keita and Takahashi, Akira and Tanaka, Isao},'
+                'journal = {Phys. Rev. B}, volume = {95}, issue = {14}, '
+                'pages = {144110}, year = {2017}, publisher = {American Physic'
+                'al Society}, doi = {10.1103/PhysRevB.95.144110}}']
+
+    def implementors(self):
+        return ["Maxwell Dylla", "Saurabh Bajaj"]
+
+
+class AngularFourierSeries(BaseFeaturizer):
+    """
+    Compute the angular fourier series (AFS) for a site. The AFS includes
+    both radial and angular information about site neighbors. The AFS is the
+    product of distance functionals (g_n, g_n') between two pairs of atoms
+    (sharing the common central site) and the cosine of the angle between the
+    two pairs. The AFS is a 2-dimentional feature (the axes are g_n, g_n').
+
+    Examples of distance functionals are square functions, gaussians, trig
+    functions, and bessel functions. An example for gaussians:
+        lambda d: exp( -(d - d_n)**2 ), where d_n is the coefficient for g_n
+
+    Args:
+        bins:   (list of touples) a list of (str, functions). The str is a text
+                 label for each bin functional. The functions should accept
+                 scalar numpy arrays (each scalar value corresponds to a
+                 distance) and return arrays of floats.
+                  (e.g. lambda d: exp( a_0 * (d - b_0)**2 ))
+        cutoff: (float) maximum distance to look for neighbors. The
+                 featurizer will run slowly for large distance cutoffs
+                 because of the number of neighbor pairs scales as
+                 the square of the number of neighbors
+    """
+
+    def __init__(self, bins, cutoff=10.0):
+        self.bins = bins
+        self.cutoff = cutoff
+
+    def featurize(self, struct, idx):
+        """
+        Get AFS of the input structure.
+        Args:
+            struct (Structure): Pymatgen Structure object.
+            idx (int): index of target site in structure struct.
+
+        Returns:
+            Flattened list of AFS values. the list order is:
+                g_n g_n'
+        """
+
+        if not struct.is_ordered:
+            raise ValueError("Disordered structure support not built yet")
+
+        # Generate list of neighbor position vectors (relative to central
+        # atom) and distances from each central site as touples
+        sites = struct._sites
+        central_site = sites[idx]
+        neighbors_lst = struct.get_neighbors(central_site, self.cutoff)
+        neighbor_collection = [
+            (neighbor[0].coords - central_site.coords, neighbor[1])
+            for neighbor in neighbors_lst]
+
+        # Generate exaustive permutations of neighbor pairs around each central
+        # site (order matters). Does not allow repeat elements (i.e. there
+        # are two distinct sites in every permutation)
+        neighbor_touples = itertools.permutations(neighbor_collection, 2)
+
+        # Generate cos(theta) between neighbor pairs for each central site.
+        # Also, retain data on neighbor distances for each pair
+        # process with matrix algebra, we really need the speed here
+        data = np.array(list(neighbor_touples))
+        v1, v2 = np.vstack(data[:, 0, 0]), np.vstack(data[:, 1, 0])
+        distances = data[:, :, 1]
+        neighbor_pairs = np.concatenate([
+            np.clip(np.einsum('ij,ij->i', v1, v2) /
+                    np.linalg.norm(v1, axis=1) /
+                    np.linalg.norm(v2, axis=1), -1.0, 1.0).reshape(-1, 1),
+            distances], axis=1)
+
+        # Generate distance functional matrix (g_n, g_n')
+        bin_combos = list(itertools.product(self.bins, repeat=2))
+
+        # Compute AFS values for each element of the bin matrix
+        # need to cast arrays as floats to use np.exp
+        cos_angles, dist1, dist2 = neighbor_pairs[:, 0].astype(float),\
+            neighbor_pairs[:, 1].astype(float),\
+            neighbor_pairs[:, 2].astype(float)
+        features = [sum(combo[0][1](dist1) * combo[1][1](dist2) *
+                        cos_angles) for combo in bin_combos]
+
+        return features
+
+    def feature_labels(self):
+        bin_combos = list(itertools.product(self.bins, repeat=2))
+        return ['bin {} bin {}'.format(combo[0][0], combo[1][0])
+                for combo in bin_combos]
+
+    def citations(self):
+        return ['@article{PhysRevB.95.144110, title = {Representation of compo'
+                'unds for machine-learning prediction of physical properties},'
+                ' author = {Seko, Atsuto and Hayashi, Hiroyuki and Nakayama, '
+                'Keita and Takahashi, Akira and Tanaka, Isao},'
+                'journal = {Phys. Rev. B}, volume = {95}, issue = {14}, '
+                'pages = {144110}, year = {2017}, publisher = {American Physic'
+                'al Society}, doi = {10.1103/PhysRevB.95.144110}}']
+
+    def implementors(self):
+        return ["Maxwell Dylla"]
