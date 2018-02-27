bdb756c7f1d4597dc30266ba467b7682a95c279d
ardunn <ardunn@lbl.gov>
2018-02-27 14:33:53 -0800

    Add simple violin example

--- matminer/figrecipes/examples/violin.py
+++ matminer/figrecipes/examples/violin.py
@@ -1,1 +1,18 @@
 # PlotlyFig violin examples will go here
+
+
+from matminer import PlotlyFig
+from matminer.datasets.dataframe_loader import load_elastic_tensor
+
+
+df = load_elastic_tensor()
+
+def simple_violin():
+    pf = PlotlyFig(df, title="Distribution of Elastic Constant Averages",
+                   colorscale='Reds')
+    pf.violin(cols=['K_Reuss', 'K_Voigt', 'G_Reuss', 'G_Voigt'],
+              use_colorscale=True)
+
+
+if __name__ == "__main__":
+    simple_violin()
