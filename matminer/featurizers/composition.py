54ccf9d7f72e37d1dc0db9933332d2ebacce44dc
sbajaj <saurabhbajaj2@gmail.com>
2018-04-27 10:39:40 -0700

    remove Kittel reference

--- matminer/featurizers/composition.py
+++ matminer/featurizers/composition.py
@@ -940,11 +940,7 @@ class CohesiveEnergy(BaseFeaturizer):
     def citations(self):
         # TODO: @sbajaj unclear whether cohesive energies are taken from first ref, second ref, or combination of both
         return [
-            "@misc{, title = {{Knowledgedoor Cohesive energy handbook}}, "
-            "url = {http://www.knowledgedoor.com/2/elements_handbook/cohesive_energy.html}}",
-            "@book{Kittel, author = {Kittel, C}, isbn = {978-0-471-41526-8}, "
-            "publisher = {Wiley}, title = {{Introduction to Solid State "
-            "Physics, 8th Edition}}, year = {2005}}"]
+            "@misc{ url = {http://www.knowledgedoor.com/2/elements_handbook/cohesive_energy.html}}"]
 
 
 class Miedema(BaseFeaturizer):
