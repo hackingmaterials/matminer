1ac75216ffc6d9cb55bc3f1be61c76df301eaf4a
Alireza <alireza@wustl.edu>
2018-05-15 18:43:46 -0700

    adjust tests to new input formats

--- matminer/data_retrieval/tests/test_retrieve_Citrine.py
+++ matminer/data_retrieval/tests/test_retrieve_Citrine.py
@@ -22,12 +22,16 @@ class CitrineDataRetrievalTest(unittest.TestCase):
         pifs_lst = self.cdr.get_api_data(formula="W", data_type='EXPERIMENTAL',
                                          max_results=10)
         self.assertEqual(len(pifs_lst), 10)
-        df = self.cdr.get_dataframe(formula='W', data_type='EXPERIMENTAL',
-                                    max_results=10)
+        df = self.cdr.get_dataframe(criteria={'formula':'W',
+                                              'data_type':'EXPERIMENTAL',
+                                              'max_results':10},
+                                    print_properties_options=False)
         self.assertEqual(df.shape[0], 10)
 
     def test_multiple_items_in_list(self):
-        df = self.cdr.get_dataframe(data_set_id=114192, max_results=102)
+        df = self.cdr.get_dataframe(criteria={'data_set_id': 114192,
+                                              'max_results':102},
+                                    print_properties_options=False)
         self.assertEqual(df.shape[0], 102)
         test_cols = {"Thermal conductivity_5-conditions", "Condition_1",
                      "Thermal conductivity_10"}
