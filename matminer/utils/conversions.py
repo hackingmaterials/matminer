9d7708934999c07610e3f1addd35e4751b655cdd
ardunn <ardunn@lbl.gov>
2018-06-08 12:36:04 -0700

    update docs for homogenize_multiindex

--- matminer/utils/conversions.py
+++ matminer/utils/conversions.py
@@ -6,30 +6,32 @@ from pymatgen import Composition
 from pymatgen.core.structure import IStructure
 
 
-def homogenize_multiindex(df, coerce=False):
+def homogenize_multiindex(df, default_key, coerce=False):
     """
-    Homogenizes a dataframe column index to a 2-layer multiindex.
+    Homogenizes a dataframe column index to a 2-level multiindex.
 
     Args:
         df (pandas DataFrame): A dataframe
-        coerce (bool): If True, try to force a 2+ level multiindex to a 2-layer
+        default_key (str): The key to use when a single Index must be converted
+            to a 2-level index. This key is then used as a parent of all
+            keys present in the original 1-level index.
+        coerce (bool): If True, try to force a 2+ level multiindex to a 2-level
             multiindex.
 
     Returns:
         df (pandas DataFrame): A dataframe with a 2-layer multiindex.
     """
-    nlevels = df.columns.nlevels
     if not isinstance(df.columns, pd.MultiIndex):
-        cols = pd.MultiIndex.from_product((["Input Data"], df.columns.values))
+        cols = pd.MultiIndex.from_product(([default_key], df.columns.values))
         df.columns = cols
         return df
-    elif nlevels == 2:
+    elif df.columns.nlevels == 2:
         return df
     elif coerce:
         # Drop levels lower than the base column indices
         warnings.warn("Multiindex has nlevels more than 2! Coercing...")
-        l1 = df.columns.get_level_values(nlevels - 1)
-        l2 = df.columns.get_level_values(nlevels - 2)
+        l1 = df.columns.get_level_values(df.columns.nlevels - 1)
+        l2 = df.columns.get_level_values(df.columns.nlevels - 2)
         cols = pd.MultiIndex.from_arrays((l2, l1))
         df.columns = cols
         return df
