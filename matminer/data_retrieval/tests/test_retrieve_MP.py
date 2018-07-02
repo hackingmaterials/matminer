57fbbf7cc7c6cfb99c2bb76c4d16deef5d00b717
Alireza <alireza.faghaninia@gmail.com>
2018-07-02 13:28:02 -0700

    add tests for retrieving bandstructure or dos

--- matminer/data_retrieval/tests/test_retrieve_MP.py
+++ matminer/data_retrieval/tests/test_retrieve_MP.py
@@ -6,6 +6,9 @@ import unittest
 from unittest import SkipTest
 
 from matminer.data_retrieval.retrieve_MP import MPDataRetrieval
+from pymatgen.electronic_structure.bandstructure import BandStructureSymmLine, \
+    BandStructure
+from pymatgen.electronic_structure.dos import CompleteDos
 
 
 class MPDataRetrievalTest(unittest.TestCase):
@@ -15,8 +18,19 @@ class MPDataRetrievalTest(unittest.TestCase):
     def test_get_data(self):
         if self.mpdr.mprester.api_key:
             df = self.mpdr.get_dataframe(criteria={"material_id": "mp-23"},
-                                         properties=["structure"])
+                                         properties=["structure",
+                                                     "bandstructure",
+                                                     "dos"])
             self.assertEqual(len(df["structure"]), 1)
+            self.assertEqual(df["bandstructure"][0].get_band_gap()["energy"],0)
+            self.assertTrue(isinstance(df["bandstructure"][0], BandStructureSymmLine))
+            self.assertTrue(isinstance(df["dos"][0], CompleteDos))
+
+            df = self.mpdr.get_dataframe(criteria={"material_id": "mp-23"},
+                                         properties=["bandstructure"],
+                                         line_mode=False)
+            self.assertTrue(isinstance(df["bandstructure"][0], BandStructure))
+
         else:
             raise SkipTest("Skipped MPDataRetrieval test; no MAPI_KEY detected")
 
