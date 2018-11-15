e45d3c941f465c7b9dec14133c9e5678d1f68a5b
Daniel Dopp <dbdopp@lbl.gov>
2018-11-15 14:41:59 -0800

    - Added elastic tensor data generation script - Adjusted mp generation script to add oxidation data and convert strings to Pymatgen objects

--- dev_scripts/dataset_management/generate_datasets.py
+++ dev_scripts/dataset_management/generate_datasets.py
@@ -2,18 +2,68 @@ from time import sleep
 
 import pandas as pd
 from tqdm import tqdm
+from pymatgen.core.structure import Structure
+from pymatgen.core.composition import Composition
 
 from matminer.utils.io import store_dataframe_as_json
+from matminer.featurizers.conversions import DictToObject, \
+    StructureToOxidStructure, StrToComposition, CompositionToOxidComposition
 from matminer.data_retrieval.retrieve_MP import MPDataRetrieval, MPRestError
 
 
+def convert_to_oxide_structure(df, structure_col_name="structure"):
+    """
+    Takes a dataframe with a pymatgen Structure column and adds oxidation data
+    to it. If structure is not a proper object, will try to convert first.
+
+    Args:
+        df (pd.DataFrame): DataFrame with a column of Structure objects
+
+        structure_col_name (str): The identifier of the structure column
+
+    Returns: (pd.DataFrame)
+    """
+    if not isinstance(df[structure_col_name][0], Structure):
+        df = DictToObject(
+            target_col_id=structure_col_name, overwrite_data=True
+        ).featurize_dataframe(df, structure_col_name)
+
+    df = StructureToOxidStructure(
+        target_col_id=structure_col_name, overwrite_data=True
+    ).featurize_dataframe(df, structure_col_name)
+
+    return df
+
+
+def convert_to_oxide_composition(df, composition_col_name="composition"):
+    """
+    Takes a dataframe with a pymatgen Composition column and adds oxidation data
+    to it. If composition is not a proper object, will try to convert first.
+
+    Args:
+        df (pd.DataFrame): DataFrame with a column of Composition objects
+
+        composition_col_name (str): The identifier of the composition column
+
+    Returns: (pd.DataFrame)
+    """
+    if not isinstance(df[composition_col_name][0], Composition):
+        df = StrToComposition(
+            target_col_id=composition_col_name, overwrite_data=True
+        ).featurize_dataframe(df, composition_col_name)
+
+    df = CompositionToOxidComposition(
+        target_col_id=composition_col_name, overwrite_data=True
+    ).featurize_dataframe(df, composition_col_name)
+
+    return df
+
+
 # Functions for generating static datasets from various database resources
-
-
 def generate_mp(max_nsites=None, properties=None, write_to_csv=False,
                 write_to_compressed_json=True):
     """
-    Grabs all mp materials. This will return two csv/json.gz files:
+    Grabs all mp materials. This will create two csv/json.gz files:
         * mp_nostruct: All MP materials, not including structures
         * mp_all: All MP materials, including structures
 
@@ -29,7 +79,7 @@ def generate_mp(max_nsites=None, properties=None, write_to_csv=False,
             dataframe to json.gz file
 
     Returns (pandas.DataFrame):
-        retrieved/generated data
+        retrieved/generated data without structure data
     """
 
     # Set default properties if None and ensure is a list
@@ -71,6 +121,11 @@ def generate_mp(max_nsites=None, properties=None, write_to_csv=False,
 
         df = df.append(site_response)
 
+    # Convert returned data to the appropriate
+    # pymatgen Structure/Composition objects
+    df = convert_to_oxide_structure(df)
+    df = convert_to_oxide_composition(df)
+
     tqdm.write("DataFrame with {} entries created".format(len(df)))
 
     # Write data out to file if user so chooses
@@ -87,5 +142,95 @@ def generate_mp(max_nsites=None, properties=None, write_to_csv=False,
     return df
 
 
+def generate_elastic_tensor(write_to_csv=False, write_to_compressed_json=True):
+    """
+    Grabs all materials with elasticity data.
+    This will return a csv/json.gz file:
+        * mp_all: All MP materials, including structures
+
+    Args:
+        write_to_csv (bool): whether to write resulting dataframe to csv
+
+        write_to_compressed_json (bool): whether to write resulting
+            dataframe to json.gz file
+
+    Returns (pandas.DataFrame):
+        retrieved/generated data
+    """
+
+    # ignore mp-978085 as causes Python to
+    # crash when performing Voronoi analysis.
+    criteria = {
+        "elasticity": {"$exists": True},
+        'material_id': {'$ne': 'mp-978085'}
+    }
+    properties = ['structure', 'pretty_formula',
+                  'elasticity.K_VRH', 'elasticity.G_VRH',
+                  'elasticity.warnings'
+                  ]
+
+    df = pd.DataFrame()
+    mpdr = MPDataRetrieval()
+
+    # Iterate over each site number to ensure return object isn't too large
+    for site_num in tqdm([i for i in range(1, 101)] + [{"$gt": 100}],
+                         desc="Querying MP"):
+        criteria["nsites"] = site_num
+        # While loop to repeat queries if server request fails
+        while True:
+            try:
+                site_response = mpdr.get_dataframe(
+                    criteria=criteria, properties=properties, index_mpid=True
+                )
+                break
+
+            except MPRestError:
+                tqdm.write("Error querying materials project, "
+                           "trying again after 5 sec")
+                sleep(5)
+
+        df = df.append(site_response)
+
+    df.rename(columns={'elasticity.K_VRH': 'K_VRH',
+                       'elasticity.G_VRH': 'G_VRH'},
+              index=str, inplace=True)
+
+    tqdm.write("There are {} elastic entries on MP".format(
+        df['K_VRH'].count()
+    ))
+
+    df = df[~(df["elasticity.warnings"].apply(bool))]
+
+    df = df.drop(["elasticity.warnings"], axis=1)
+
+    tqdm.write("There are {} elastic entries on MP with no warnings".format(
+        df['K_VRH'].count()
+    ))
+
+    print(df.describe())
+    print(df.head())
+
+    # Convert returned data to the appropriate
+    # pymatgen Structure/Composition objects
+    df = convert_to_oxide_structure(df)
+    df = convert_to_oxide_composition(df)
+
+    print(df.describe())
+    print(df.head())
+
+    # Write data out to file if user so chooses
+    if write_to_csv:
+        df.to_csv("elastic_tensor_2018.csv")
+
+    if write_to_compressed_json:
+        store_dataframe_as_json(
+            df, "elastic_tensor_2018.json.gz", compression="gz"
+        )
+
+    return df
+
+
 if __name__ == "__main__":
-    generate_mp()
+    generate_mp(write_to_csv=True, write_to_compressed_json=True)
+    # generate_elastic_tensor()
+
