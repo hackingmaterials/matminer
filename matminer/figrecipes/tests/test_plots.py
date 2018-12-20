3b20bdf89fe5d2b1fe662dc15240e3cda4b3a662
ardunn <ardunn@lbl.gov>
2018-12-19 19:46:59 -0800

    update troublesome key to prevent circleci's rounding error of ["xaxis"]["range"] of each vio

--- matminer/figrecipes/tests/test_plots.py
+++ matminer/figrecipes/tests/test_plots.py
@@ -159,12 +159,15 @@ class PlotlyFigTest(PymatgenTest):
         pcp_true = self.fopen("template_pcp.json")
         self.assertTrue(pcp_test == pcp_true)
 
-    # @unittest.skip("some weird plotly thing? see: https://github.com/hackingmaterials/matminer/issues/348")
     def test_violin(self):
         vio_test = self.pf.violin(
             [a, b, c, b, a, c, b], cols=xlabels, return_plot=True)['layout']
         vio_test = vio_test.to_plotly_json()
         vio_true = self.fopen("template_vio.json")
+
+        # Avoid errors from CircleCI's different plotly config
+        for vio in [vio_test, vio_true]:
+            vio["xaxis"]["range"] = [-0.167009, 0.167009]
         self.assertDictEqual(vio_test, vio_true)
 
     def test_scatter_matrix(self):
