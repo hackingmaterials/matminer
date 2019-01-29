bbb739dc631d3eec3341fadbca10df377c7624bb
Maxwell Dylla <maxwelldylla2020@u.northwestern.edu>
2019-01-29 12:14:04 -0800

    change requirement handling in setup
    requirements are now stored in yaml files. citrine client has been changed to optional feature. the optional requirements are parsed as requirements for testing.

--- setup.py
+++ setup.py
@@ -1,31 +1,75 @@
 #!/usr/bin/env python
 
 import os
+import yaml
 
 from setuptools import setup, find_packages
 
-module_dir = os.path.dirname(os.path.abspath(__file__))
-reqs_raw = open(os.path.join(module_dir, "requirements.txt")).read()
-reqs_list = [r.replace("==", ">=") for r in reqs_raw.split("\n")]
+
+PWD = os.path.dirname(os.path.abspath(__file__))
+
+
+def parse_requirements(requirement_type):
+    """Parses installation, optional, and testing requirements from YAML files.
+
+    The "optional" requirements are required for testing. The installation
+    requirements are stored in "requirements.yaml" and the optional/testing
+    requirements are stored in "requirements-optional.yaml".
+
+    Args:
+        requirement_type: (str) Either "install", "optional", or "testing".
+
+    Returns: (list or dict) Either a list of dependencies (install/testing) or
+        a dictionary of dependencies (optional).
+    """
+    if requirement_type == 'install':
+
+        with open(os.path.join(PWD, "requirements.yaml")) as file:
+            requirements = yaml.load(file)
+            return [r.replace("==", ">=") for r in requirements]
+
+    else:
+
+        with open(os.path.join(PWD, "requirements-optional.yaml")) as file:
+            optional_requirements = yaml.load(file)
+
+            if requirement_type == 'optional':
+
+                for key, val in optional_requirements.items():
+                    optional_requirements[key] = [
+                        r.replace("==", ">=") for r in val]
+                return optional_requirements
+
+            elif requirement_type == 'testing':
+
+                testing_requires = []
+                for val in optional_requirements.values():
+                    testing_requires.extend(
+                        [r.replace("==", ">=") for r in val])
+                return testing_requires
+
 
 if __name__ == "__main__":
     setup(
         name='matminer',
         version='0.5.0',
-        description='matminer is a library that contains tools for data mining in Materials Science',
-        long_description=open(os.path.join(module_dir, 'README.md')).read(),
+        description='matminer is a library that contains tools for data mining'
+                    ' in Materials Science',
+        long_description=open(os.path.join(PWD, 'README.md')).read(),
         url='https://github.com/hackingmaterials/matminer',
         author='Anubhav Jain',
         author_email='anubhavster@gmail.com',
         license='modified BSD',
         packages=find_packages(),
-        package_data={'matminer.datasets': ['*.json'],
-                      'matminer.featurizers': ["*.yaml"],
-                      'matminer.utils.data_files': ['*.csv', '*.tsv', '*.json', 'magpie_elementdata/*.table', 'jarvis/*.json']},
+        package_data={
+            'matminer.datasets': ['*.json'],
+            'matminer.featurizers': ["*.yaml"],
+            'matminer.utils.data_files': ['*.csv', '*.tsv', '*.json',
+                                          'magpie_elementdata/*.table',
+                                          'jarvis/*.json']},
         zip_safe=False,
-        install_requires=reqs_list,
-        extras_require={'mpds': ['jmespath>=0.9.3', 'ujson>=1.35', 'httplib2>=0.10.3', 'ase>=3.14.1'],
-                        'mdf': ['mdf_forge==0.6.1']},
+        install_requires=parse_requirements('install'),
+        extras_require=parse_requirements('optional'),
         classifiers=['Programming Language :: Python :: 2.7',
                      'Programming Language :: Python :: 3.6',
                      'Development Status :: 4 - Beta',
@@ -36,7 +80,8 @@ if __name__ == "__main__":
                      'Topic :: Other/Nonlisted Topic',
                      'Topic :: Scientific/Engineering'],
         test_suite='nose.collector',
-        tests_require=['nose'],
+        tests_require=parse_requirements('testing').append('nose'),
         scripts=[]
-        # scripts=[os.path.join('scripts', f) for f in os.listdir(os.path.join(module_dir, 'scripts'))]
-)
+        # scripts=[os.path.join('scripts', f) for f in
+        #          os.listdir(os.path.join(module_dir, 'scripts'))]
+    )
