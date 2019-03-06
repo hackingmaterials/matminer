93b2b92aec261fe4377e0a2defd2ef558b50cf44
Samy Cherfaoui <scherfaoui@berkeley.edu>
2019-03-06 13:00:24 -0800

    Updated inplace == False code

--- matminer/featurizers/base.py
+++ matminer/featurizers/base.py
@@ -260,13 +260,12 @@ class BaseFeaturizer(BaseEstimator, TransformerMixin):
 
         if inplace:
             # Update the existing dataframe
-            for k in labels:
-                df[k] = res[k]
+            df[labels] = res[labels]
             return df
         else:
             # Create new dataframe and ensure columns are ordered properly
             new = pd.concat([df, res], axis=1)
-            return new[df.columns.tolist() + res.columns.tolist()]
+            return new[list(df).extend(list(res))]
 
     def _generate_column_labels(self, multiindex, return_errors):
         """Create a list of column names for a dataframe
