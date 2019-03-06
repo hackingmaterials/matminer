018782b57c8e1bc971c40e1629c3cabfc59b7f4b
Samy Cherfaoui <scherfaoui@berkeley.edu>
2019-03-06 14:42:32 -0800

    Fixed tolist() issue

--- matminer/featurizers/tests/test_conversions.py
+++ matminer/featurizers/tests/test_conversions.py
@@ -107,7 +107,7 @@ class TestConversions(TestCase):
         sto = StructureToOxidStructure(target_col_id='structure_oxid2',
                                        oxi_states_override={"Cl": [-2],
                                                             "Cs": [+2]})
-        df = sto.featurize_dataframe(df, 'structure')
+        df = sto.featurize_dataframe(df, 'structure', inplace=True)
         self.assertEqual(df["structure_oxid2"].tolist()[0][0].specie.oxi_state,
                          -2)
         self.assertEqual(df["structure_oxid2"].tolist()[0][1].specie.oxi_state,
@@ -187,7 +187,7 @@ class TestConversions(TestCase):
                                                    df_2lvl.columns.values))
 
         df_2lvl = StrToComposition().featurize_dataframe(
-            df_2lvl, ("custom", "comp_str"), multiindex=True)
+            df_2lvl, ("custom", "comp_str"), inplace=True, multiindex=True)
         self.assertEqual(df_2lvl[("StrToComposition", "composition")].tolist(),
                          [Composition("Fe2"), Composition("MnO2")])
 
