1cd7b2e83729149d1ee5a88dccd0775072961caf
Samy Cherfaoui <scherfaoui@berkeley.edu>
2019-03-06 13:12:20 -0800

    Let's try this

--- matminer/featurizers/base.py
+++ matminer/featurizers/base.py
@@ -265,7 +265,7 @@ class BaseFeaturizer(BaseEstimator, TransformerMixin):
         else:
             # Create new dataframe and ensure columns are ordered properly
             new = pd.concat([df, res], axis=1)
-            return new[df.columns.tolist() + res.columns.tolist()]
+            return new[labels]
 
     def _generate_column_labels(self, multiindex, return_errors):
         """Create a list of column names for a dataframe
