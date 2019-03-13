f76c069ada549f961078d272f5954195ba16c760
Samy Cherfaoui <scherfaoui@berkeley.edu>
2019-03-13 15:03:12 -0700

    We are getting closer

--- matminer/featurizers/tests/test_conversions.py
+++ matminer/featurizers/tests/test_conversions.py
@@ -119,7 +119,7 @@ class TestConversions(TestCase):
         # test in-place
         sto = StructureToOxidStructure(target_col_id=None, overwrite_data=True)
         df = sto.featurize_dataframe(df, 'structure')
-        print(type(df["structure"]))
+        print(df["structure"])
         self.assertEqual(df["structure"].tolist()[0][0].specie.oxi_state, -1)
 
         # test error handling
