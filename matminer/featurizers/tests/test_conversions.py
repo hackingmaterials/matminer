cbd429a9fe1f8b648f8e88e2b443223a0153f152
Samy Cherfaoui <scherfaoui@berkeley.edu>
2019-03-13 16:02:03 -0700

    Hopefully final debug commit

--- matminer/featurizers/tests/test_conversions.py
+++ matminer/featurizers/tests/test_conversions.py
@@ -126,7 +126,7 @@ class TestConversions(TestCase):
         df = sto.featurize_dataframe(df, 'structure')
         print(type(df["structure"]))
         print(list(df["structure"]))
-        print(type(df["structure"].iloc[0]["structure"]))
+        print(df["structure"].iloc[0]["structure"].iloc[0])
         self.assertEqual(df["structure"].tolist()[0][0].specie.oxi_state, -1)
 
         # test error handling
