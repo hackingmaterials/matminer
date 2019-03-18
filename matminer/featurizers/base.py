489b2d9042aa685c87b30d409e044037f14e2495
Samy Cherfaoui <scherfaoui@berkeley.edu>
2019-03-18 01:54:00 -0700

    Test

--- matminer/featurizers/base.py
+++ matminer/featurizers/base.py
@@ -265,7 +265,7 @@ class BaseFeaturizer(BaseEstimator, TransformerMixin):
         else:
             # Create new dataframe and ensure columns are ordered properly
             new = pd.concat([df, res], axis=1)
-            if getattr(self, '_target_col_id', None):
+            if getattr(self, '_target_col_id', col_id):
                 return new[df.columns.tolist() + res.columns.tolist()][col_id]
             else:
                 return new[df.columns.tolist() + res.columns.tolist()]
