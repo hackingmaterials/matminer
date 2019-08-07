32f6224ff8bf438cb9af20b1c0d53fb32163ebb7
ardunn <ardunn@lbl.gov>
2019-08-07 12:40:32 -0700

    change test_GlobalInstabilityIndex to expectedfailure

--- matminer/featurizers/tests/test_structure.py
+++ matminer/featurizers/tests/test_structure.py
@@ -863,9 +863,8 @@ class StructureFeaturesTest(PymatgenTest):
         self.assertTupleEqual(df.shape, (3, 451))
         self.assertAlmostEqual(df["SOAP_449"].iloc[1], 3.029167413711548, places=5)
 
-    # todo: remove skip once https://github.com/materialsproject/pymatgen/pull/1548 is versioned into PMG
-    @unittest.skip(reason="Pymatgen bv_sum interally broken, will be fixed "
-                          "in next PMG release")
+    # todo: remove skip once pymatgen version newer than 2019.7.30 is released.
+    @unittest.expectedFailure
     def test_GlobalInstabilityIndex(self):
         # Test diamond and ni3al fail precheck
         gii = GlobalInstabilityIndex(r_cut=4.0, disordered_pymatgen=False)
