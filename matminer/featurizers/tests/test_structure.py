d19dad06fbbc1fde5d31ec6a3c5dbe2df42f3678
ardunn <ardunn@lbl.gov>
2019-08-07 12:23:04 -0700

    fix skip problem

--- matminer/featurizers/tests/test_structure.py
+++ matminer/featurizers/tests/test_structure.py
@@ -864,7 +864,8 @@ class StructureFeaturesTest(PymatgenTest):
         self.assertAlmostEqual(df["SOAP_449"].iloc[1], 3.029167413711548, places=5)
 
     # todo: remove skip once https://github.com/materialsproject/pymatgen/pull/1548 is versioned into PMG
-    @unittest.skip()
+    @unittest.skip(reason="Pymatgen bv_sum interally broken, will be fixed "
+                          "in next PMG release")
     def test_GlobalInstabilityIndex(self):
         # Test diamond and ni3al fail precheck
         gii = GlobalInstabilityIndex(r_cut=4.0, disordered_pymatgen=False)
