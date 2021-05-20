8fe6bc4fb4ce28254f4180d3caa51588759aac51
Evgeny Blokhin <eb@tilde.pro>
2021-05-21 00:21:37 +0200

    Remove outdated *setting* field, add deprecation comment, and polish a bit

--- matminer/data_retrieval/retrieve_MPDS.py
+++ matminer/data_retrieval/retrieve_MPDS.py
@@ -1,3 +1,9 @@
+"""
+Warning:
+This retrieval class is to be deprecated in favor of the *mpds_client* library
+`pip install mpds_client` (https://pypi.org/project/mpds-client),
+which is fully compatible with matminer
+"""
 from __future__ import division
 
 import os
@@ -68,19 +74,19 @@ class MPDSDataRetrieval(BaseDataRetrieval):
     jsonobj = client.get_data(
         {"formula":"SrTiO3", "sgs": 99, "props":"atomic properties"},
         fields={
-            'S':["entry", "cell_abc", "sg_n", "setting", "basis_noneq", "els_noneq"]
+            'S':["entry", "cell_abc", "sg_n", "basis_noneq", "els_noneq"]
         }
     )
 
     *or*
-    jsonobj = client.get_data({"formula":"SrTiO3"}, fields=[])
+    jsonobj = client.get_data({"formula":"SrTiO3"}, fields={})
 
     If you use this data retrieval class, please additionally cite:
     Blokhin, E., Villars, P., 2018. The PAULING FILE Project and Materials
     Platform for Data Science: From Big Data Toward Materials Genome,
-    in: Andreoni, W., Yip, S. (Eds.), Handbook of Materials Modeling :
+    in: Andreoni, W., Yip, S. (Eds.), Handbook of Materials Modeling:
     Methods: Theory and Modeling. Springer International Publishing, Cham,
-    pp. 1–26. https://doi.org/10.1007/978-3-319-42913-7_62-1
+    pp. 1-26. https://doi.org/10.1007/978-3-319-42913-7_62-2
 
     """
     default_properties = ('Phase', 'Formula', 'SG',
@@ -90,7 +96,7 @@ class MPDSDataRetrieval(BaseDataRetrieval):
 
     pagesize = 1000
     maxnpages = 100  # NB one hit may reach 50kB in RAM, consider pagesize*maxnpages*50kB free RAM
-    chillouttime = 3  # NB please, do not use values < 3, because the server may burn out
+    chillouttime = 2  # NB please, do not use values < 2
 
     def __init__(self, api_key=None, endpoint=None):
         """
@@ -272,32 +278,31 @@ class MPDSDataRetrieval(BaseDataRetrieval):
 
         Attention! These two flavors are not compatible, e.g.
         primitive vs. crystallographic cell is defaulted,
-        atoms wrapped or non-wrapped into the unit cell etc.
+        atoms wrapped or non-wrapped into the unit cell, etc.
 
         Note, that the crystal structures are not retrieved by default,
         so one needs to specify the fields while retrieval:
             - cell_abc
             - sg_n
-            - setting
             - basis_noneq
             - els_noneq
-        e.g. like this: {'S':['cell_abc', 'sg_n', 'setting', 'basis_noneq', 'els_noneq']}
+        e.g. like this: {'S':['cell_abc', 'sg_n', 'basis_noneq', 'els_noneq']}
         NB. occupancies are not considered.
 
         Args:
             datarow: (list) Required data to construct crystal structure:
-                [cell_abc, sg_n, setting, basis_noneq, els_noneq]
+                [cell_abc, sg_n, basis_noneq, els_noneq]
             flavor: (str) Either "pmg", or "ase"
 
         Returns:
-            - if flavor is pmg, returns Pymatgen Structure object
-            - if flavor is ase, returns ASE Atoms object
+            - if flavor is pmg, Pymatgen Structure object
+            - if flavor is ase, ASE Atoms object
         """
         if not datarow or not datarow[-1]:
             return None
 
-        cell_abc, sg_n, setting, basis_noneq, els_noneq = \
-            datarow[-5], int(datarow[-4]), datarow[-3], datarow[-2], datarow[-1]
+        cell_abc, sg_n, basis_noneq, els_noneq = \
+            datarow[-4], int(datarow[-3]), datarow[-2], datarow[-1]
 
         if flavor == 'pmg':
             return Structure.from_spacegroup(
@@ -309,7 +314,6 @@ class MPDSDataRetrieval(BaseDataRetrieval):
 
         elif flavor == 'ase' and use_ase:
             atom_data = []
-            setting = 2 if setting == '2' else 1
 
             for num, i in enumerate(basis_noneq):
                 atom_data.append(Atom(els_noneq[num], tuple(i)))
@@ -319,7 +323,6 @@ class MPDSDataRetrieval(BaseDataRetrieval):
                 spacegroup=sg_n,
                 cellpar=cell_abc,
                 primitive_cell=True,
-                setting=setting,
                 onduplicates='replace'
             )
 
