371fcfb55bfdcab49db01f889f6c8de4cbaea970
ardunn <ardunn@lbl.gov>
2021-06-28 22:00:13 -0700

    fix linting

--- matminer/data_retrieval/tests/test_retrieve_MongoDB.py
+++ matminer/data_retrieval/tests/test_retrieve_MongoDB.py
@@ -3,17 +3,11 @@ import unittest
 from pymongo import MongoClient
 from pymatgen.util.testing import PymatgenTest
 
-from matminer.data_retrieval.retrieve_MongoDB import \
-    clean_projection, \
-    remove_ints, \
-    MongoDataRetrieval
+from matminer.data_retrieval.retrieve_MongoDB import clean_projection, remove_ints, MongoDataRetrieval
 from matminer.data_retrieval.tests.base import on_ci
-
-import pandas as pd
 
 
 class MongoDataRetrievalTest(PymatgenTest):
-
     def test_cleaned_projection(self):
         p = ["n.o.e", "n.o.e.l", "a.b", "a.b.c", "m", "m.b"]
         result = clean_projection(p)
@@ -27,20 +21,18 @@ class MongoDataRetrievalTest(PymatgenTest):
         self.assertEqual(remove_ints("a.1"), "a")
         self.assertEqual(remove_ints("a.1.x"), "a.x")
 
-    @unittest.skipIf(
-        not on_ci,
-        "MongoDataRetrievalTest configured only to run on CI by default"
-    )
+    @unittest.skipIf(not on_ci, "MongoDataRetrievalTest configured only to run on CI by default")
     def test_get_dataframe(self):
         db = MongoClient("localhost", 27017, username="admin", password="password").test_db
         c = db.test_collection
         docs = [
-            {"some": {"nested": {"result": 14.5}},
-             "other": "notnestedresult",
-             "final": 16.938475 + i,
-             "array": [1.4, 5.6, 11.2, 1.1],
-             "valid": True
-             }
+            {
+                "some": {"nested": {"result": 14.5}},
+                "other": "notnestedresult",
+                "final": 16.938475 + i,
+                "array": [1.4, 5.6, 11.2, 1.1],
+                "valid": True,
+            }
             for i in range(5)
         ]
 
@@ -50,8 +42,7 @@ class MongoDataRetrievalTest(PymatgenTest):
         mdr = MongoDataRetrieval(c)
 
         df = mdr.get_dataframe(
-            criteria={"valid": True},
-            properties=["some.nested.result", "other", "final", "array", "valid"]
+            criteria={"valid": True}, properties=["some.nested.result", "other", "final", "array", "valid"]
         )
 
         self.assertTrue((df["some.nested.result"] == 14.5).all())
@@ -62,9 +53,5 @@ class MongoDataRetrievalTest(PymatgenTest):
 
         self.assertArrayAlmostEqual(df["array"].iloc[0], [1.4, 5.6, 11.2, 1.1])
         self.assertTrue(df["valid"].all())
-
-        pd.set_option('display.max_rows', 500)
-        pd.set_option('display.max_columns', 500)
-        pd.set_option('display.width', 1000)
 
         c.drop()
