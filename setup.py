466898539bc2ae41ff922ad4e8e1750f07e2ff5b
ardunn <ardunn@lbl.gov>
2021-06-28 21:31:16 -0700

    package_data not needed if MANIFEST specified and include_package_data=True

--- setup.py
+++ setup.py
@@ -37,12 +37,6 @@ if __name__ == "__main__":
         author_email='anubhavster@gmail.com',
         license='modified BSD',
         packages=find_packages(),
-        package_data={
-            'matminer.datasets': ['*.json'],
-            'matminer.featurizers': ["*/*.yaml"],
-            'matminer.utils.data_files': ['*.cif', '*.csv', '*.tsv', '*.json',
-                                          'magpie_elementdata/*.table',
-                                          'jarvis/*.json', '*.txt']},
         include_package_data=True,
         zip_safe=False,
         install_requires=reqs_list,
