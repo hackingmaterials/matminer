f4f450ad07547125bc5390ce91f895a92554114a
Patrick Huck <phuck@lbl.gov>
2022-11-10 14:42:12 -0800

    work on release through github actions (#885)
    * use scm for version
    * local version override

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import os
 
 from setuptools import find_packages, setup
 
+
+def local_version(version):
+    # https://github.com/pypa/setuptools_scm/issues/342
+    return ""
+
+
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
 extras_require = {
@@ -22,7 +28,12 @@ tests_require = [r for v in extras_require.values() for r in v]
 if __name__ == "__main__":
     setup(
         name='matminer',
-        version='0.7.8',
+        use_scm_version={
+            "root": ".",
+            "relative_to": __file__,
+            "local_scheme": local_version,
+        },
+        setup_requires=["setuptools_scm"],
         description='matminer is a library that contains tools for data '
                     'mining in Materials Science',
         long_description=open(os.path.join(module_dir, 'README.md')).read(),
