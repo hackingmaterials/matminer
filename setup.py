a7a53c7f23f53ba1f5458b6d290c20621ed1f489
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-07 13:24:19 +0800

    clear long description as readme

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
-import os
 
 from setuptools import setup
 
-module_dir = os.path.dirname(os.path.abspath(__file__))
-
-
 if __name__ == "__main__":
     setup(
-        long_description=open(os.path.join(module_dir, "README.md")).read(),
-        long_description_content_type="text/markdown",
         zip_safe=False,
-        scripts=[],
     )
